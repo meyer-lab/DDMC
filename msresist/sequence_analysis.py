6c26c3000113bf3bbdf1c660308f43598160d4f6
marcc <creixell@ucla.edu>
2019-06-30 02:55:31 -0700

    filter based on variability

--- msresist/sequence_analysis.py
+++ msresist/sequence_analysis.py
@@ -7,46 +7,162 @@ import random
 ###------------ Sequence processor ------------------###
 
 def GenerateFastaFile(PathToFile, PN, X_seqs):
-    FileHandle = open(FileName, "w+")
+    FileHandle = open(PathToFile, "w+")
     for i in range(len(X_seqs)):
         FileHandle.write('>'+ str(PN[i]))
         FileHandle.write("\n")
         FileHandle.write(str(X_seqs[i]))
         FileHandle.write("\n")
+    FileHandle.close()
+    
 
+###------------ Dictionary to Check Motifs ------------------###        
+
+ """Input: Phosphopeptide sequences 
+    Output: Dictionary to see all sequences categorized by singly or doubly phosphorylated.
+    Useful to check def GeneratingKinaseMotifs results 
+ """
+
+def YTSsequences(X_seqs):
+    YTSsequences = {}
+    seq1 , seq2, seq3, seq4, seq5, seq6, = [], [], [], [], [], []
+    for i, seq in enumerate(X_seqs):
+        if "y" in seq and "t" not in seq and "s" not in seq:
+            seq1.append(seq)
+        if "t" in seq and "y" not in seq and "s" not in seq:
+            seq2.append(seq)
+            DictProtNameToPhospho["t: "] = seq2
+        if "s" in seq and "y" not in seq and "t" not in seq:
+            seq3.append(seq)
+            DictProtNameToPhospho["s: "] = seq3
+        if "y" in seq and "t" in seq and "s" not in seq:
+            seq4.append(seq)
+            DictProtNameToPhospho["y/t: "] = seq4
+        if "y" in seq and "s" in seq and "t" not in seq:
+            seq5.append(seq)
+            DictProtNameToPhospho["y/s: "] = seq5
+        if "t" in seq and "s" in seq and "y" not in seq:
+            seq6.append(seq)
+
+    DictProtNameToPhospho["y: "] = seq1
+    DictProtNameToPhospho["t: "] = seq2
+    DictProtNameToPhospho["s: "] = seq3
+    DictProtNameToPhospho["y/t: "] = seq4
+    DictProtNameToPhospho["y/s: "] = seq5
+    DictProtNameToPhospho["t/s: "] = seq6
+
+    SeqsBySites = pd.DataFrame(dict([ (k, pd.Series(v)) for k,v in DictProtNameToPhospho.items() ]))
+    
+    return SeqsBySites
+
+
+###------------ Match protein names from MS to Uniprot's data set ------------------###        
+
+ """Input: Path to new file and MS fasta file  
+    Output: Protein names list and kinase motif list. Run with def GenerateFastaFile to obtain the final file
+    Kinase motif -5 +5 wrt the phosphorylation site. It accounts for doubly phosphorylated peptides (lowercase y, t, s)
+ """
+
+
+def MatchProtNames(PathToNewFile, MS_seqs)
+#     FileHandle = open("./msresist/data/MS_seqs_matched.fa", "w+")
+    FileHandle = open(PathToNewFile, "w+")
+    #counter = 0
+    for rec1 in SeqIO.parse(MS_seqs, "fasta"):
+        MS_seq = str(rec1.seq)
+        MS_seqU = str(rec1.seq.upper())
+        MS_name = str(rec1.description.split(" OS")[0])
+        try:
+            UP_seq = DictProtToSeq_UP[MS_name]
+            FileHandle.write(">" + MS_name)
+            FileHandle.write("\n")
+            FileHandle.write(MS_seq)
+            FileHandle.write("\n")
+        except:
+            #counter += 1
+            Fixed_name = getKeysByValue(DictProtToSeq_UP, MS_seqU)
+            FileHandle.write(">" + Fixed_name[0])
+            FileHandle.write("\n")
+            FileHandle.write(MS_seq)
+            FileHandle.write("\n")       
+    FileHandle.close()
+
+    
+###------------ Generate Phosphopeptide Motifs ------------------###        
+
+ """Input: Fasta file and Uniprot's proteome dictionary key: Protein accession value: protein sequence
+    Output: Protein names list and kinase motif list. Run with def GenerateFastaFile to obtain the final file
+    Kinase motif -5 +5 wrt the phosphorylation site. It accounts for doubly phosphorylated peptides (lowercase y, t, s)
+ """
+
+
+def GeneratingKinaseMotifs(PathToFile, DictProtToSeq_UP):
+    ExtSeqs = []
+    MS_names = []
+    for rec1 in SeqIO.parse(MS_seqs_matched, "fasta"):
+        MS_seq = str(rec1.seq)
+        MS_seqU = str(rec1.seq.upper())
+        MS_name = str(rec1.description.split(" OS")[0])
+        MS_names.append(MS_name)
+        try:
+            UP_seq = DictProtToSeq_UP[MS_name] 
+            if MS_seqU in UP_seq and MS_name == list(DictProtToSeq_UP.keys())[list(DictProtToSeq_UP.values()).index(UP_seq)]:
+                counter += 1
+                regexPattern = re.compile(MS_seqU)
+                MatchObs = regexPattern.finditer(UP_seq)
+                indices = []
+                for i in MatchObs:
+                    indices.append(i.start())   #VHLENATEYAtLR   #YNIANtV
+                    indices.append(i.end())                 
+                if "y" in MS_seq and "t" not in MS_seq and "s" not in MS_seq:
+                    y_idx = MS_seq.index("y") + indices[0]
+                    ExtSeqs.append(UP_seq[y_idx-5:y_idx] + "y" + UP_seq[y_idx+1:y_idx+6])
+
+                if "t" in MS_seq and "y" not in MS_seq and "s" not in MS_seq:
+                    t_idx = MS_seq.index("t") + indices[0]
+                    ExtSeqs.append(UP_seq[t_idx-5:t_idx] + "t" + UP_seq[t_idx+1:t_idx+6])
+
+                if "s" in MS_seq and "y" not in MS_seq and "t" not in MS_seq:
+                    s_idx = MS_seq.index("s") + indices[0]
+                    ExtSeqs.append(UP_seq[s_idx-5:s_idx] + "s" + UP_seq[s_idx+1:s_idx+6])
+
+                if "y" in MS_seq and "t" in MS_seq and "s" not in MS_seq:     
+                    y_idx = MS_seq.index("y") + indices[0]                  
+                    ExtSeq = UP_seq[y_idx-5:y_idx] + "y" + UP_seq[y_idx+1:y_idx+6]
+                    y_idx = MS_seq.index("y")
+                    if "t" in MS_seq[y_idx-5:y_idx+6]:
+                        t_idx = MS_seq[y_idx-5:y_idx+6].index("t")
+                        ExtSeqs.append(ExtSeq[:t_idx] + "t" + ExtSeq[t_idx+1:])
+                    else:
+                        ExtSeqs.append(ExtSeq)
+
+                if "y" in MS_seq and "s" in MS_seq and "t" not in MS_seq:
+                    y_idx = MS_seq.index("y") + indices[0]
+                    ExtSeq = UP_seq[y_idx-5:y_idx] + "y" + UP_seq[y_idx+1:y_idx+6]
+                    y_idx = MS_seq.index("y")
+                    if "s" in MS_seq[y_idx-5:y_idx+6]:
+                        s_idx = MS_seq[y_idx-5:y_idx+6].index("s")
+                        ExtSeqs.append(ExtSeq[:s_idx] + "s" + ExtSeq[s_idx+1:])
+                    else:
+                        ExtSeqs.append(ExtSeq)
+
+                if "t" in MS_seq and "s" in MS_seq and "y" not in MS_seq:
+                    t_idx = MS_seq.index("t") + indices[0]
+                    ExtSeq = UP_seq[t_idx-5:t_idx] + "t" + UP_seq[t_idx+1:t_idx+6]
+                    t_idx = MS_seq.index("t")
+                    if "s" in MS_seq[t_idx-5:t_idx+6]:
+                        s_idx = MS_seq[t_idx-5:t_idx+6].index("s")
+                        ExtSeqs.append(ExtSeq[:s_idx] + "s" + ExtSeq[s_idx+1:])
+                    else:
+                        ExtSeqs.append(ExtSeq)
+        except:
+            print("find and replace", MS_name, "in proteome_uniprot.txt. Use: ", MS_seq)
+            pass
         
-###------------ Mapping to Uniprot's proteome and Extension of Phosphosite Sequences ------------------###
-# TO DO: include names manually and account for peptides with multiple y,t,s... use MS_seq
-        
-DictProtToSeq_UP = {}
-for rec2 in SeqIO.parse(proteome, "fasta"):
-    UP_seq = str(rec2.seq)
-    UP_name = rec2.description.split("HUMAN ")[1].split(" OS")[0]
-    DictProtToSeq_UP[UP_name] = str(UP_seq)
+        return MS_names, ExtSeqs
 
+###------------ Mapping to Uniprot's proteome and Extension of Phosphosite Sequences ------------------###        
 
-ExtSeq = []
-counter = 0
-for rec1 in SeqIO.parse(MS_seqs, "fasta"):
-    MS_seq = str(rec1.seq)
-    MS_seqU = str(rec1.seq.split("-")[0].upper())
-    MS_name = str(rec1.description.split(" OS")[0])
-    try:
-        UP_seq = DictProtToSeq_UP[MS_name]
-        if MS_seqU in UP_seq and MS_name == list(DictProtToSeq_UP.keys())[list(DictProtToSeq_UP.values()).index(UP_seq)]:
-            counter += 1
-            regexPattern = re.compile(MS_seqU)
-            MatchObs = regexPattern.finditer(UP_seq)
-            indices = []
-            for i in MatchObs:
-                indices.append(i.start())
-                indices.append(i.end())
-            ExtSeq.append(UP_seq[indices[0]:indices[1]])
-    except:
-        print("find ", MS_name, "in proteome_uniprot.txt and change name")
-        pass
-
-###------------ Sequence processor ------------------###
 # Code from Adam Weiner, obtained March 2019
 
 def trim(seqFile):
@@ -54,30 +170,34 @@ def trim(seqFile):
     homeDir = cwd[1:5]
     if (homeDir == 'home'):
         print('using path from server to load sequences')
-        pathToFile = os.path.join("/home", "adamw", "flu-vax", "fluv", str(seqFile))  # aretha server
-    else:
-        print('using path from windows machine to load sequences')
-        pathToFile = './trial_seq.fa'
-        # pathToFile = os.path.join(Users","Marc","Documents","flu-vax", str(seqFile))  #windows machine
+#         pathToFile = os.path.join("/home","zoekim","Desktop",str(seqFile)) #aretha server
+        pathToFile = os.path.join("/home", "marcc", "resistance-MS", "msresist", "data", str(seqFile))     #/home/marcc/resistance-MS/msresist/data
 
+    else:
+        print('using path from mac machine to load sequences')
+        pathToFile = os.path.join("/Users","zoekim","Desktop",str(seqFile))  #mac machine
+    
     allSeqs = []
     allLabels = []
     for seq_record in SeqIO.parse(pathToFile, """fasta"""):
-        allSeqs.append(seq_record.seq)
-        allLabels.append(seq_record.id)
-
+            allSeqs.append(seq_record.seq)
+            allLabels.append(seq_record.id)
+    
     seqMat = np.array(allSeqs)
     label = np.array(allLabels)
-
+    
     sequence = seqMat[:, 0:317]
-
+    
     # filtering out residues not included in PAM250 pymsa distance matrix (http://www.matrixscience.com/blog/non-standard-amino-acid-residues.html)
     for i in range(0, sequence.shape[0]):
-        for j in range(0, sequence.shape[1]):
-            if (sequence[i, j] == 'J'):
-                sequence[i, j] = random.choice(['I', 'L'])
-
+        for j in range(0,sequence.shape[1]):
+            if (sequence[i,j] == 'J'):
+                sequence[i,j] = random.choice(['I', 'L'])
+    print(label)
+    print(sequence)
+    
     return (label, sequence)
+
 
 
 ###------------ Seq Distance Calculator ------------------###
@@ -105,9 +225,9 @@ class Distance:
                 if self.subMat is "PAM250":  # convert log-scaled PAM250 values to true values
                     temp_dist = np.exp(temp_dist)
                 dist[ii] = 1 / temp_dist  # large distances have small values in matrices
-            avg_dist = np.sum(dist) / 317.0
+#             avg_dist = np.sum(dist) / 317.0
 
-            return avg_dist
+            return np.sum(dist)
 
     def test_mat(self):
         """ function is the same as "dist_mat()" except that it only looks at first 10 sequences
@@ -125,6 +245,8 @@ class Distance:
 
     def dist_mat(self):
         distMat = np.zeros((self.numSeq, self.numSeq))
+        print(self.numSeq)
+        print(self.sequences.shape)
         print('calculating the full distance matrix based on PAM250')
         for i in range(0, self.numSeq):
             for j in range(i, self.numSeq):
@@ -151,7 +273,8 @@ class SubstitutionMatrix:
         :param char1: First character.
         :param char2: Second character.
         :return: the distance value """
-
+        print(char1)
+        print(char2)
         if char1 is self.gap_character and char2 is self.gap_character:
             distance = 1
         elif char1 is self.gap_character or char2 is self.gap_character:
@@ -329,3 +452,4 @@ class FLU_sub(SubstitutionMatrix):
 
     def get_distance_matrix(self) -> dict:
         return self.distance_matrix
+
