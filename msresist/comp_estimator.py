2ecc76a03262854f9413900a0630136683de861e
Aaron Meyer <ameyer@ucla.edu>
2019-07-29 08:12:01 -0700

    First file.
    Former-commit-id: 6406394dc0700bdd670245927dd11044dc3be8bf [formerly d0cdfd3083e6781a46236c45b60dff571c7132c0] [formerly 4459855e840f063bdcaae0ed444b863e4e5de306]
    Former-commit-id: c08607d065e1a8b3871d3fdb277997999a5c70dd
    Former-commit-id: 22636da2ac303da45f84b2da2707633418568d12
    Former-commit-id: 1d519bcc7175fb544c68b2bce6b9641e78fc19e4

--- msresist/comp_estimator.py
+++ msresist/comp_estimator.py
@@ -11,36 +11,62 @@ from sklearn.pipeline import Pipeline
 
 
 class MyOwnKMEANS(BaseEstimator):
-    
+
     def __init__(self, n_clusters, ProtNames, peptide_phosphosite):
         self.n_clusters = n_clusters
         self.ProtNames = ProtNames
         self.peptide_phosphosite = peptide_phosphosite
-    
+
     def fit(self, X, Y):
         self.kmeans_ = KMeans(n_clusters=self.n_clusters).fit(np.transpose(X))
         return self
 
     def transform(self, X):
         centers, DictClusterToMembers = ClusterAverages(np.array(X), self.kmeans_.labels_, self.n_clusters, X.shape[0], self.ProtNames, self.peptide_phosphosite)
-        return centers 
-    
+        return centers
+
     def ClusterMembers(self, X):
         centers, DictClusterToMembers = ClusterAverages(np.array(X), self.kmeans_.labels_, self.n_clusters, X.shape[0], self.ProtNames, self.peptide_phosphosite)
         return DictClusterToMembers
-                                                              
-                                                              
+
+
 ###------------ Building Pipeline and Tunning Hyperparameters ------------------###
 
-def ComHyperPar(X,Y, ProtNames, peptide_phosphosite):
+def ComHyperPar(X, Y, ProtNames, peptide_phosphosite):
     estimators = [('kmeans', MyOwnKMEANS(5, ProtNames, peptide_phosphosite)), ('plsr', PLSRegression(2))]
     pipe = Pipeline(estimators)
-    param_grid = dict(kmeans__n_clusters = [2], plsr__n_components = [1,2]), dict(kmeans__n_clusters = [3], plsr__n_components = np.arange(1,4)), dict(kmeans__n_clusters = [4], plsr__n_components = np.arange(1,5)), dict(kmeans__n_clusters = [5], plsr__n_components = np.arange(1,6)), dict(kmeans__n_clusters = [6], plsr__n_components = np.arange(1,7)), dict(kmeans__n_clusters = [7], plsr__n_components = np.arange(1,8)), dict(kmeans__n_clusters = [8], plsr__n_components = np.arange(1,9)), dict(kmeans__n_clusters = [9], plsr__n_components = np.arange(1,10)), dict(kmeans__n_clusters = [10], plsr__n_components = np.arange(1,11)), dict(kmeans__n_clusters = [11], plsr__n_components = np.arange(1,12)), dict(kmeans__n_clusters = [12], plsr__n_components = np.arange(1,13)), dict(kmeans__n_clusters = [13], plsr__n_components = np.arange(1,14)), dict(kmeans__n_clusters = [14], plsr__n_components = np.arange(1,15)), dict(kmeans__n_clusters = [15], plsr__n_components = np.arange(1,16))
+    param_grid = dict(
+        kmeans__n_clusters=[2], plsr__n_components=[
+            1, 2]), dict(
+        kmeans__n_clusters=[3], plsr__n_components=np.arange(
+            1, 4)), dict(
+        kmeans__n_clusters=[4], plsr__n_components=np.arange(
+            1, 5)), dict(
+        kmeans__n_clusters=[5], plsr__n_components=np.arange(
+            1, 6)), dict(
+        kmeans__n_clusters=[6], plsr__n_components=np.arange(
+            1, 7)), dict(
+        kmeans__n_clusters=[7], plsr__n_components=np.arange(
+            1, 8)), dict(
+        kmeans__n_clusters=[8], plsr__n_components=np.arange(
+            1, 9)), dict(
+        kmeans__n_clusters=[9], plsr__n_components=np.arange(
+            1, 10)), dict(
+        kmeans__n_clusters=[10], plsr__n_components=np.arange(
+            1, 11)), dict(
+        kmeans__n_clusters=[11], plsr__n_components=np.arange(
+            1, 12)), dict(
+        kmeans__n_clusters=[12], plsr__n_components=np.arange(
+            1, 13)), dict(
+        kmeans__n_clusters=[13], plsr__n_components=np.arange(
+            1, 14)), dict(
+        kmeans__n_clusters=[14], plsr__n_components=np.arange(
+            1, 15)), dict(
+        kmeans__n_clusters=[15], plsr__n_components=np.arange(
+            1, 16))
     grid = GridSearchCV(pipe, param_grid=param_grid, cv=X.shape[0], return_train_score=True, scoring='neg_mean_squared_error')
     fit = grid.fit(X, Y)
     CVresults_max = pd.DataFrame(data=fit.cv_results_)
-    std_scores = { '#Clusters': CVresults_max['param_kmeans__n_clusters'], '#Components': CVresults_max['param_plsr__n_components'], 'mean_test_scores': CVresults_max["mean_test_score"], 'mean_train_scores': CVresults_max["mean_train_score"]}
+    std_scores = {'#Clusters': CVresults_max['param_kmeans__n_clusters'], '#Components': CVresults_max['param_plsr__n_components'], 'mean_test_scores': CVresults_max["mean_test_score"], 'mean_train_scores': CVresults_max["mean_train_score"]}
     CVresults_min = pd.DataFrame(data=std_scores)
     return CVresults_max, CVresults_min, fit.best_params_
-
-                                                              
