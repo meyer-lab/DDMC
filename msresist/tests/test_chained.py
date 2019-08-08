d4f850fb2c9c4809de8bfba4187fa0aa3b0d6a4f
marcc <creixell@ucla.edu>
2019-08-08 15:14:42 -0700

    linting.

--- msresist/tests/test_chained.py
+++ msresist/tests/test_chained.py
@@ -2,30 +2,25 @@
 Testing file for the chained methods.
 """
 import unittest
+import random
+import string
 import numpy as np
 from sklearn.datasets import load_diabetes
 from sklearn.cluster import KMeans
 from ..plsr import ClusterAverages
-import random
-import string
-
 
 class TestEstimator(unittest.TestCase):
     """ Testing class for a chained KMeans/PLSR estimator. """
 
     def test_fitting(self):
         """ Test that ClusterAverages is working by comparing to cluster centers. """
-
         X = load_diabetes(return_X_y=False)['data']
-
         MadeUp_ProtNames, MadeUp_peptide_phosphosite = [], []
         for i in range(X.shape[0]):
             MadeUp_ProtNames.append(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10)))
             MadeUp_peptide_phosphosite.append(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10)))
         n_clusters = 3
-
         kmeans = KMeans(init="k-means++", n_clusters=n_clusters)
-
         cluster_assignments = kmeans.fit_predict(X.T)
         centers, DictClusterToMembers = ClusterAverages(X, cluster_assignments, n_clusters, X.shape[0], MadeUp_ProtNames, MadeUp_peptide_phosphosite)
 
