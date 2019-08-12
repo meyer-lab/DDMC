dc8c60e346a7741b48e496cf9681d7e4addc9313
marcc <creixell@ucla.edu>
2019-08-12 11:45:16 -0700

    Fig1 edits + best fit line in MeasuredVsPredicted plot.
    Former-commit-id: d22a8a70aec45d7d4fde80692ccaf3c052c2e416 [formerly fc9db3d8f43af5e85d3c5fca22b13e15a91f976a] [formerly 7288c3910a9db17144f929479d0472a023982acb]
    Former-commit-id: e6d316f1583548cdcdc66bc71f39b061216d94bb
    Former-commit-id: 43719a845d8eb1be51ac9c15f4be5a0ddbf830ef
    Former-commit-id: 9307c162967389520f92e033de503cadaa443bbe

--- msresist/plsr.py
+++ msresist/plsr.py
@@ -3,6 +3,7 @@
 import scipy as sp
 from scipy.stats import zscore
 import numpy as np
+from numpy.polynomial.polynomial import polyfit
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import cross_val_predict, LeaveOneOut
@@ -84,6 +85,7 @@ def MeasuredVsPredicted_LOOCVplot(X, Y, plsr_model, fig, ax, axs):
     print("Pearson's R: ", coeff, "\n", "p-value: ", pval)
     if ax == "none":
         plt.scatter(Y, np.squeeze(Y_predictions))
+        plt.plot(np.unique(Y), np.poly1d(np.polyfit(Y, np.squeeze(Y_predictions), 1))(np.unique(Y)), color = "r")
         plt.title("Correlation Measured vs Predicted")
         plt.xlabel("Measured Cell Viability")
         plt.ylabel("Predicted Cell Viability")
