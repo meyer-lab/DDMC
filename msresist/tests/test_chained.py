baad5cf6de5efecacc9c7f3fdea90feb159cf54f
marcc <creixell@ucla.edu>
2019-08-16 14:55:17 -0700

    linting
    Former-commit-id: d880ff9e12ae867e9367b74f3812a1e63bfbcae3 [formerly f1a8e4dad3cec8a6a4818a0fb5a1727d1c35cea4] [formerly be3e751ee78556517bbc873e8d293abc3e984a95 [formerly 8728f6a0abd31367ff1dfe495a96e1358315ba87] [formerly 0f3c33587e3a18593d1721b992e16b7712d63e92]]
    Former-commit-id: be3e751ee78556517bbc873e8d293abc3e984a95 [formerly 8728f6a0abd31367ff1dfe495a96e1358315ba87]
    Former-commit-id: be3e751ee78556517bbc873e8d293abc3e984a95
    Former-commit-id: 1de4f92b8d23a0d851b346d3211f6afccebea3e9
    Former-commit-id: 1cfb0ee9730e743f57cd679cb8498e829802eebe
    Former-commit-id: d8bc32360305a51e1378e9e3351b41636d9879c6

--- msresist/tests/test_chained.py
+++ msresist/tests/test_chained.py
@@ -2,8 +2,6 @@
 Testing file for the chained methods.
 """
 import unittest
-import random
-import string
 import numpy as np
 import pandas as pd
 from sklearn.datasets import load_diabetes
@@ -18,7 +16,7 @@ class TestEstimator(unittest.TestCase):
         X = pd.DataFrame(load_diabetes(return_X_y=False)['data'])
         kmeans = KMeans(init="k-means++", n_clusters=4)
         cluster_assignments = kmeans.fit_predict(X.T)
-        centers, DictClusterToMembers = ClusterAverages(X, cluster_assignments)
+        centers = ClusterAverages(X, cluster_assignments)
 
         # Assert that the cluster centers are also their averages from assignments
-        self.assertTrue(np.allclose(np.array(kmeans.cluster_centers_).T, centers))
+        self.assertTrue(np.allclose(np.array(kmeans.cluster_centers_).T, centers[0]))
