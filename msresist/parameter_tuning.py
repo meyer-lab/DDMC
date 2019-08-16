c22a267cd6b94d234c7af7ba24ed2415d48d7289
marcc <creixell@ucla.edu>
2019-08-16 14:25:01 -0700

    code simplifications and organization.
    Former-commit-id: 67e0a05e34e2196b8be8539a3587255dcf4722ef [formerly e0aa488e5708754c893ff3e14da3cf7526fb5ff2] [formerly c7bd42a5f6313de33dff1657d0b497c3f4b8879d]
    Former-commit-id: 7a53ecf5c8b0fef14c6065a7ebadfbf05a0ed879
    Former-commit-id: b69c4f1e16ef559c99a3a6b640f28597bacb5f86
    Former-commit-id: be71e26038096edea75c37800fc0072bc3d01f0f

---
+++ msresist/parameter_tuning.py
@@ -0,0 +1,37 @@
+"Hyperparameter Tuning using GridSearch"
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from msresist.clustering import MyOwnKMEANS
+from sklearn.cross_decomposition import PLSRegression
+
+
+###------------ Building k-means + PLSR pipeline and Tunning Hyperparameters ------------------###
+
+def ComHyperPar(X, Y):
+    """ Cross-validation: Simultaneous hyperparameter search for number of clusters and number of components """
+    estimators = [('kmeans', MyOwnKMEANS(5)), ('plsr', PLSRegression(2))]
+    pipe = Pipeline(estimators)
+
+    param_grid = []
+    for nn in range(2, 16):
+        param_grid.append(dict(kmeans__n_clusters=[nn], plsr__n_components=list(np.arange(1, nn + 1))))
+
+    grid = GridSearchCV(pipe, param_grid=param_grid, cv=X.shape[0], return_train_score=True, scoring='neg_mean_squared_error')
+    fit = grid.fit(X, Y)
+    CVresults_max = pd.DataFrame(data=fit.cv_results_)
+    std_scores = {'#Clusters': CVresults_max['param_kmeans__n_clusters'], '#Components': CVresults_max['param_plsr__n_components'], 'mean_test_scores': CVresults_max["mean_test_score"], 'mean_train_scores': CVresults_max["mean_train_score"]}
+    return CVresults_max, pd.DataFrame(data=std_scores), fit.best_params_
+
+
+
+###------------ General GridSearch Structure ------------------###
+
+def GridSearch_CV(model, X, Y, parameters, cv, scoring=None):
+    """ Exhaustive search over specified parameter values for an estimator. """
+    grid = GridSearchCV(model, param_grid=parameters, cv=cv, scoring=scoring)
+    fit = grid.fit(X, Y)
+    CVresults_max = pd.DataFrame(data=fit.cv_results_)
+    return CVresults_max
