06b28e3a7ea8e112e0757c41e1707a31b01d3651
marcc <creixell@ucla.edu>
2019-09-05 18:11:35 -0700

    update poster figures.

--- msresist/plsr.py
+++ msresist/plsr.py
@@ -59,3 +59,36 @@ def plotMeasuredVsPredicted(ax, plsr_model, X, Y):
     textstr = "$r$ = " + str(np.round(coeff, 4))
     props = dict(boxstyle='square', facecolor='none', alpha=0.5, edgecolor='black')
     ax.text(0.80, 0.09, textstr, transform=ax.transAxes, fontsize=10, verticalalignment='top', bbox=props);
+
+
+""" Avoid data labels from overlapping. """
+# source: https://stackoverflow.com/questions/8850142/matplotlib-overlapping-annotations
+
+def get_text_positions(x_data, y_data, txt_width, txt_height):
+    a = zip(y_data, x_data)
+    text_positions = y_data.copy()
+    for index, (y, x) in enumerate(a):
+        local_text_positions = [i for i in a if i[0] > (y - txt_height)
+                            and (abs(i[1] - x) < txt_width * 2) and i != (y,x)]
+        if local_text_positions:
+            sorted_ltp = sorted(local_text_positions)
+            if abs(sorted_ltp[0][0] - y) < txt_height: #True == collision
+                differ = np.diff(sorted_ltp, axis=0)
+                a[index] = (sorted_ltp[-1][0] + txt_height, a[index][1])
+                text_positions[index] = sorted_ltp[-1][0] + txt_height
+                for k, (j, m) in enumerate(differ):
+                    #j is the vertical distance between words
+                    if j > txt_height * 1.5: #if True then room to fit a word in
+                        a[index] = (sorted_ltp[k][0] + txt_height, a[index][1])
+                        text_positions[index] = sorted_ltp[k][0] + txt_height
+                        break
+    return text_positions
+
+
+def text_plotter(x_data, y_data, text_positions, axis,txt_width,txt_height):
+    for x,y,t in zip(x_data, y_data, text_positions):
+        axis.text(x - .03, 1.02*t, '%d'%int(y),rotation=0, color='blue', fontsize=13)
+        if y != t:
+            axis.arrow(x, t+20,0,y-t, color='blue',alpha=0.2, width=txt_width*0.0,
+                       head_width=.02, head_length=txt_height*0.5,
+                       zorder=0,length_includes_head=True)
