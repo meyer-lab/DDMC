ba28d744c8a252e566f482105de32915d5e1f987
marcc <creixell@ucla.edu>
2019-11-01 16:42:35 -0700

    fix.

--- msresist/tests/test_MSclustering.py
+++ msresist/tests/test_MSclustering.py
@@ -13,17 +13,16 @@ class TestClustering(unittest.TestCase):
         """ Test that EMclustering is working by comparing with GMM clusters. """
         ABC = preprocessing(motifs=True, Vfilter=True, FCfilter=True, log2T=True)
         ABC = preprocess_seqs(ABC, "Y")
-        data = ABC.iloc[:, 3:13].T
-        seqs = ABC.iloc[:, 0]
-        names = ABC.iloc[:, 1]
-        ncl = 4
-        GMMweight = 1
+        data = ABC.iloc[:, 6:].T
+        info = ABC.iloc[:, :6]
+        ncl = 2
+        GMMweight = 2.5
         pYTS = "Y"
         distance_method = "PAM250"
         covariance_type = "diag"
         max_n_iter = 20
         
-        Cl_seqs, _, _, _, _ = EM_clustering(data, seqs, names, ncl, GMMweight, pYTS, distance_method, covariance_type, max_n_iter)
+        Cl_seqs, _, _, _, _ = EM_clustering(info, names, ncl, GMMweight, pYTS, distance_method, covariance_type, max_n_iter)
         gmm_cl, _, _ = gmm_initialCl_and_pvalues(ABC, 4, "diag", "Y")
         gmm_cl = [[str(seq) for seq in cluster] for cluster in gmm_cl]
 
