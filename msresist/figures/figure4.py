8391871417e301a71d4503f49ce9481472641207
marcc <creixell@ucla.edu>
2020-02-15 15:18:16 -0800

    .

--- msresist/figures/figure4.py
+++ msresist/figures/figure4.py
@@ -3,7 +3,7 @@ This creates Figure 4.
 """
 from .common import subplotLabel, getSetup
 import pandas as pd
-from msresist.pre_processing import preprocessing, MergeDfbyMean 
+from msresist.pre_processing import preprocessing, preprocess_seqs, MergeDfbyMean 
 from msresist.figures.figure2 import plotR2YQ2Y, plotMeasuredVsPredicted, plotScoresLoadings, plotclusteraverages
 
 
