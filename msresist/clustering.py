3f2abd815f18110aafcfdaa1cbc4f6dcb44af758
marcc <creixell@ucla.edu>
2020-05-03 20:54:25 -0700

    again.

--- msresist/clustering.py
+++ msresist/clustering.py
@@ -80,7 +80,7 @@ class MassSpecClustering(BaseEstimator):
     def fit(self, X, _):
         """ Compute EM clustering. """
         def EM_clustering(data, info, ncl, GMMweight, distance_method, max_n_iter):
-        self.cl_seqs_, self.labels_, self.scores_, self.n_iter_ = EM_clustering_opt(X, self.info, \
+            self.cl_seqs_, self.labels_, self.scores_, self.n_iter_ = EM_clustering_opt(X, self.info,
                                             self.ncl, self.GMMweight, self.distance_method, self.max_n_iter, self.n_runs)
         return self
 
