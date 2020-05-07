d7b61a23bff2dd5c889608b7a5db6502f5a60932
Marc Creixell <creixell@g.ucla.edu>
2020-05-06 19:54:52 -0700

    Cptac binomial debugging (#141)
    * binomial fix.
    
    * organizing function in sequence_analysis.py by sequence method.
    
    * more.
    
    * more cleaning.
    
    * .predict() fix.
    
    * phenotypes PCA plot.
    
    * update requirements.txt
    
    * .
    
    * linting.
    
    * redefine weight variable.
    
    Co-authored-by: marcc <creixell@ucla.edu>
    Former-commit-id: d828c45b1892c711f4b2e3198745de12cc62d8d3 [formerly 51f4dc3291f981e6715f97e532a57ec4d41aa572] [formerly 7d0368bee774893ce4140e83ea833113bd6a0774]
    Former-commit-id: d9d96734e27b441eaef81cd81d0388d43bab2625
    Former-commit-id: 4b5e160f6835d3965df1ff31969d31cbed76e37b
    Former-commit-id: 3bc413a8357c9cbe39d416b01518983dc2efc199

--- msresist/clustering.py
+++ msresist/clustering.py
@@ -1,97 +1,44 @@
 """ Clustering functions. """
 
 import pandas as pd
-from sklearn.cluster import KMeans
-from sklearn.mixture import GaussianMixture
 from sklearn.base import BaseEstimator
 from sklearn.utils.validation import check_is_fitted
-from msresist.sequence_analysis import EM_clustering_opt
-
-
-class MyOwnKMEANS(BaseEstimator):
-    """ Runs k-means providing the centers and cluster members and sequences. """
-
-    def __init__(self, n_clusters):
-        """ Define variables. """
-        self.n_clusters = n_clusters
-
-    def fit(self, X, _):
-        """ fit data into k-means. """
-        self.kmeans_ = KMeans(n_clusters=self.n_clusters).fit(X.T)
-        return self
-
-    def transform(self, X):
-        """ calculate cluster averages. """
-        centers, _ = ClusterAverages(X, self.kmeans_.labels_)
-        return centers
-
-    def clustermembers(self, X):
-        """ generate dictionary containing peptide names and sequences for each cluster. """
-        _, clustermembers = ClusterAverages(X, self.kmeans_.labels_)
-        return clustermembers
-
-
-class MyOwnGMM(BaseEstimator):
-    """ Runs GMM providing the centers and cluster members and sequences. """
-
-    def __init__(self, n_components):
-        """ Define variables """
-        self.n_components = n_components
-
-    def fit(self, X, _):
-        """ fit data into GMM. """
-        self.gmm_ = GaussianMixture(n_components=self.n_components, covariance_type="full").fit(X.T)
-        self.labels_ = self.gmm_.predict(X.T)
-        return self
-
-    def transform(self, X):
-        """ calculate cluster averages. """
-        centers, _ = ClusterAverages(X, self.labels_)
-        return centers
-
-    def clustermembers(self, X):
-        """ generate dictionary containing peptide names and sequences for each cluster. """
-        _, clustermembers = ClusterAverages(X, self.labels_)
-        return clustermembers
-
-    def probs(self, X):
-        """ probabilities of cluster assignment. """
-        return self.gmm_.predict_proba(X.T)
-
-    def weights(self):
-        """ weights of each cluster. """
-        return self.gmm_.weights_
+from msresist.sequence_analysis import EM_clustering_opt, e_step
 
 
 class MassSpecClustering(BaseEstimator):
     """ Cluster peptides by both sequence similarity and data behavior following an
-    expectation-maximization algorithm. GMMweight specifies which method's expectation step
+    expectation-maximization algorithm. SeqWeight specifies which method's expectation step
     should have a larger effect on the peptide assignment. """
 
-    def __init__(self, info, ncl, GMMweight, distance_method, max_n_iter=100000, n_runs=5):
+    def __init__(self, info, ncl, SeqWeight, distance_method, max_n_iter=100000, n_runs=5):
         self.info = info
         self.ncl = ncl
-        self.GMMweight = GMMweight
+        self.SeqWeight = SeqWeight
         self.distance_method = distance_method
         self.max_n_iter = max_n_iter
         self.n_runs = n_runs
 
     def fit(self, X, _):
         """ Compute EM clustering. """
-        self.cl_seqs_, self.labels_, self.scores_, self.n_iter_ = EM_clustering_opt(X, self.info,
-                                                                                    self.ncl, self.GMMweight, self.distance_method, self.max_n_iter, self.n_runs)
+        self.cl_seqs_, self.labels_, self.scores_, self.n_iter_, self.gmmp = EM_clustering_opt(X, self.info, 
+                                                                                               self.ncl, 
+                                                                                               self.SeqWeight, 
+                                                                                               self.distance_method, 
+                                                                                               self.max_n_iter, 
+                                                                                               self.n_runs)
         return self
 
     def transform(self, X):
         """ calculate cluster averages. """
-        check_is_fitted(self, ["cl_seqs_", "labels_", "scores_", "n_iter_"])
+        check_is_fitted(self, ["cl_seqs_", "gmmp", "labels_", "scores_", "n_iter_"])
 
         centers, _ = ClusterAverages(X, self.labels_)
         return centers
 
     def clustermembers(self, X):
         """ generate dictionary containing peptide names and sequences for each cluster. """
-        check_is_fitted(self, ["cl_seqs_", "labels_", "scores_", "n_iter_"])
+        check_is_fitted(self, ["cl_seqs_", "gmmp", "labels_", "scores_", "n_iter_"])
 
         _, clustermembers = ClusterAverages(X, self.labels_)
         return clustermembers
@@ -99,22 +46,22 @@ class MassSpecClustering(BaseEstimator):
     def predict(self, X, _Y=None):
         """ Predict the cluster each sequence in ABC belongs to. If this estimator is gridsearched alone it
         won't work since all sequences are passed. """
-        check_is_fitted(self, ["cl_seqs_", "labels_", "scores_", "n_iter_"])
+        check_is_fitted(self, ["cl_seqs_", "gmmp", "labels_", "scores_", "n_iter_"])
 
-#         labels, _ = e_step(X, self.distance_method, self.GMMweight, self.gmmp_, self.bg_pwm_, self.cl_seqs_, self.ncl, self.Seq1Seq2ToScoreDict, self.labels_)
-        return self.labels_
+        labels, _ = e_step(X, self.cl_seqs_, self.gmmp, self.distance_method, self.SeqWeight, self.ncl)
+        return labels
 
     def score(self, X, _Y=None):
         """ Scoring method, mean of combined p-value of all peptides"""
-        check_is_fitted(self, ["cl_seqs_", "labels_", "scores_", "n_iter_"])
+        check_is_fitted(self, ["cl_seqs_", "gmmp", "labels_", "scores_", "n_iter_"])
 
-#         _, scores = e_step(X, self.distance_method, self.GMMweight, self.gmmp_, self.bg_pwm_, self.cl_seqs_, self.ncl, self.Seq1Seq2ToScoreDict, self.labels_)
-        return self.scores_
+        _, scores = e_step(X, self.cl_seqs_, self.gmmp, self.distance_method, self.SeqWeight, self.ncl)
+        return scores
 
     def get_params(self, deep=True):
         """ Returns a dict of the estimator parameters with their values. """
         return {"info": self.info, "ncl": self.ncl,
-                "GMMweight": self.GMMweight, "distance_method": self.distance_method,
+                "SeqWeight": self.SeqWeight, "distance_method": self.distance_method,
                 "max_n_iter": self.max_n_iter}
 
     def set_params(self, **parameters):
