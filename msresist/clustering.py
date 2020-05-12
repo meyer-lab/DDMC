043f10152615b761ac7cf6eb517524c11d652de6
marcc <creixell@ucla.edu>
2020-05-12 13:52:43 -0700

    fix conflicts.

--- msresist/clustering.py
+++ msresist/clustering.py
@@ -22,20 +22,13 @@ class MassSpecClustering(BaseEstimator):
 
     def fit(self, X, _):
         """ Compute EM clustering. """
-<<<<<<< HEAD
+        print(self.ncl, self.SeqWeight)
         self.cl_seqs_, self.labels_, self.scores_, self.n_iter_, self.gmmp = EM_clustering_opt(X, self.info, 
                                                                                                self.ncl, 
                                                                                                self.SeqWeight, 
                                                                                                self.distance_method,
                                                                                                self.gmm_method,
                                                                                                self.max_n_iter, 
-=======
-        self.cl_seqs_, self.labels_, self.scores_, self.n_iter_, self.gmmp = EM_clustering_opt(X, self.info,
-                                                                                               self.ncl,
-                                                                                               self.SeqWeight,
-                                                                                               self.distance_method,
-                                                                                               self.max_n_iter,
->>>>>>> 8a59a1f12811a7d8dd73a4255c735dac4d6b6567
                                                                                                self.n_runs)
         return self
 
@@ -56,17 +49,19 @@ class MassSpecClustering(BaseEstimator):
     def predict(self, X, _Y=None):
         """ Predict the cluster each sequence in ABC belongs to. If this estimator is gridsearched alone it
         won't work since all sequences are passed. """
+        print("pred")
         check_is_fitted(self, ["cl_seqs_", "gmmp", "labels_", "scores_", "n_iter_"])
 
-        labels, _ = e_step(X, self.cl_seqs_, self.gmmp, self.distance_method, self.gmm_method, self.SeqWeight, self.ncl)
+        labels, _ = e_step(X, self.cl_seqs_, self.gmmp, self.distance_method, self.SeqWeight, self.ncl)
         return labels
 
     def score(self, X, _Y=None):
+        print("score")
         """ Scoring method, mean of combined p-value of all peptides"""
         check_is_fitted(self, ["cl_seqs_", "gmmp", "labels_", "scores_", "n_iter_"])
 
-        _, scores = e_step(X, self.cl_seqs_, self.gmmp, self.distance_method, self.SeqWeight, self.ncl)
-        return scores
+        _, score = e_step(X, self.cl_seqs_, self.gmmp, self.distance_method, self.SeqWeight, self.ncl)
+        return score
 
     def get_params(self, deep=True):
         """ Returns a dict of the estimator parameters with their values. """
