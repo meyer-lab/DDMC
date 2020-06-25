3dfe4e9cc69751d71a4b0754116b8e92fc705c29
Aaron Meyer <git@ameyer.me>
2020-06-25 14:02:42 -0700

    Further simplify R2Y/Q2Y handling.

--- msresist/plsr.py
+++ msresist/plsr.py
@@ -6,27 +6,25 @@ from sklearn.metrics import explained_variance_score
 ###------------ PLSR model functions ------------------###
 
 
-def R2Y_across_components(model, X, Y, cv, max_comps):
+def R2Y_across_components(model, X, Y, cv, max_comps, crossval=False):
     """ Calculate R2Y. """
     R2Ys = []
+
     for b in range(1, max_comps):
         if cv == 1:
             model.set_params(n_components=b)
         if cv == 2:
             model.set_params(plsr__n_components=b)
-        model.fit(X, Y)
-        R2Ys.append(model.score(X, Y))
+
+        if crossval is True:
+            y_pred = cross_val_predict(model, X, Y, cv=Y.shape[0], n_jobs=-1)
+        else:
+            y_pred = model.fit(X, Y).predict(X)
+        
+        R2Ys.append(explained_variance_score(Y, y_pred))
     return R2Ys
 
 
 def Q2Y_across_components(model, X, Y, cv, max_comps):
     """ Calculate Q2Y using cros_val_predct method. """
-    Q2Ys = []
-    for b in range(1, max_comps):
-        if cv == 1:
-            model.set_params(n_components=b)
-        if cv == 2:
-            model.set_params(plsr__n_components=b)
-        y_pred = cross_val_predict(model, X, Y, cv=Y.shape[0], n_jobs=-1)
-        Q2Ys.append(explained_variance_score(Y, y_pred))
-    return Q2Ys
+    return R2Y_across_components(model, X, Y, cv, max_comps, crossval=True)
