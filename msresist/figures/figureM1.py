27b9d568a4da2decefb8f249cfc533c1b5aaa8af
Marc Creixell <creixell@g.ucla.edu>
2020-07-13 11:31:43 -0700

    Figure m1 p3 (#180)
    * PlotWinsByWeight
    
    * run full model.
    
    * only run new lines.
    
    * Remove pylint.
    
    * Remove parallel build.
    
    * Try closing pool.
    
    * Test whether the problem happens with Binomial.
    
    * Fix process pool handling.
    
    * Switch back to PAM250.
    
    * Add back pylint.
    
    * Try this.
    
    * add PCA.
    
    * fix.
    
    * add tumortype function.
    
    * update PCA plotting function.
    
    * fix PCA plot figure3.
    
    * fix import.
    
    * import StandardScaler.
    
    * fix figure indices.
    
    * remove parallel.
    
    * figure index fix.
    
    * figure index fix.
    
    * replace binomial .pmf with .cdf
    
    * figureM1 full code.
    
    * delete pickled model.
    
    * add y file.
    
    Co-authored-by: Aaron Meyer <git@ameyer.me>
    Co-authored-by: marcc <creixell@ucla.edu>
    Former-commit-id: 61f3617336327efff369af3dc3e7f69146e726bc [formerly 7c20585db3ccde936b5eeb6caac9a2776e4170de] [formerly b9998c44919890fa54f56b575e31bd175a4410b8 [formerly 134f60ce4701b7ec543b7423dfcce22b1a840696] [formerly 17025afa5130417595a60feb7de3de8df8b1ecf2]]
    Former-commit-id: b9998c44919890fa54f56b575e31bd175a4410b8 [formerly 134f60ce4701b7ec543b7423dfcce22b1a840696]
    Former-commit-id: b9998c44919890fa54f56b575e31bd175a4410b8
    Former-commit-id: 1fdf4e003eb558f3169ba16ead4a90168b31e08b
    Former-commit-id: 7ea8b12c6000af17e58ba732527fa28e7813beb3
    Former-commit-id: 8e6639bc2d9d535673027801e3b78a496d644967

--- msresist/figures/figureM1.py
+++ msresist/figures/figureM1.py
@@ -9,13 +9,15 @@ import pandas as pd
 import seaborn as sns
 from scipy.stats import zscore
 from sklearn.metrics import mean_squared_error
+from sklearn.cross_decomposition import PLSRegression
+from msresist.figures.figure3 import plotR2YQ2Y, plotPCA
 from msresist.clustering import MassSpecClustering
 from msresist.pre_processing import filter_NaNpeptides
 
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((10, 8), (2, 4))
+    ax, f = getSetup((12.5, 12), (4, 3))
     X = pd.read_csv("msresist/data/MS/CPTAC/CPTAC-preprocessedMotfis.csv").iloc[:, 1:]
 
     d = X.select_dtypes(include=["float64"]).T
@@ -43,10 +45,79 @@ def makeFigure():
     weights = np.arange(0, 1.1, 0.1)
     PlotWinsByWeight(ax[6], i_w, d_w, weights, distance_method, ncl)
 
+    #Leave blank for heatmap of cluster centers across patients
+    ax[8].axis('off')
+
+    #Run model
+    X_f = filter_NaNpeptides(X, cut=0.1)
+    d_f = X_f.select_dtypes(include=['float64']).T
+    i_f = X_f.select_dtypes(include=['object'])
+    distance_method = "PAM250"
+    ncl = 15
+    SeqWeight = 0.20
+    MSC = MassSpecClustering(i_f, ncl, SeqWeight=SeqWeight, distance_method=distance_method, n_runs=1).fit(d_f, "NA")
+    centers = MSC.transform(d_f)
+    centers["Patient_ID"] = X.columns[4:]
+
+    #PCA of model
+    centers.iloc[:, :-1] = zscore(centers.iloc[:, :-1], axis=1)
+    centers = TumorType(centers)
+    c = 2
+    plotPCA(
+        ax[7:11], centers, c, ["Patient_ID", "Type"], "Cluster", hue_scores="Type", style_scores="Type", hue_load="Cluster"
+    )
+
+    #Regress against survival
+    centers, y = TransformCPTACdataForRegression(MSC, d_f, list(X.columns[4:]))
+
+    centers_T = centers[~centers["Patient_ID"].str.endswith(".N")].set_index("Patient_ID")
+    centers_N = centers[centers["Patient_ID"].str.endswith(".N")].set_index("Patient_ID")
+    y_T = y[~y["Patient_ID"].str.endswith(".N")].set_index("Patient_ID")
+    y_N = y[y["Patient_ID"].str.endswith(".N")].set_index("Patient_ID")
+
+    plsr = PLSRegression(n_components=2, scale=True)
+    plotR2YQ2Y(ax[11], plsr, centers_T, y_T, 1, 10)
+
     # Add subplot labels
     subplotLabel(ax)
 
     return f
+
+def FormatWhiteNames(X):
+    """Keep only the gene name."""
+    genes = []
+    counter = 0
+    for v in X.iloc[:, 0]:
+        if "GN" not in v:
+            counter += 1
+            continue
+        genes.append(v.split("GN=")[1].split(" PE")[0].strip())
+    print("number of proteins without gene name:", counter)
+    return genes
+
+def FindMatchingPeptides(X, Y, cols=False):
+    """Return peptides within the entire CPTAC LUAD data set also present in the White data or vice versa. Note
+    that if the white lab data set is used as X, the patient labels should be passed."""
+    if cols:
+        X = X[cols]
+    X = X.dropna().sort_values(by="Sequence")
+    X = X.set_index(["Gene", "Sequence"])
+    rows = []
+    counter = 0
+    for idx in range(Y.shape[0]):
+        try:
+            r = X.loc[Y["Gene"][idx], Y["Sequence"][idx]].reset_index()
+            if len(r) > 1:
+                rows.append(pd.DataFrame(r.iloc[0, :]).T)
+            else:
+                rows.append(r)
+        except:
+            counter += 1
+            continue
+    print("Number of mismatches: ", counter)
+
+    y = pd.concat(rows)
+    return y.drop_duplicates(list(y.columns), keep="first")
 
 def PlotMissingnessDensity(ax, d):
     """Plot amount of missingness per peptide."""
@@ -139,14 +210,6 @@ def PlotAMwins(ax, X, weights):
         ax[i].get_legend().remove()
     ax[3].legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0, labelspacing=0.2)
 
-def FindWinIntegers(won):
-    """Convert wins to integers"""
-    seqWin = int(won.split("SeqWins: ")[1].split(" DataWins:")[0])
-    dataWin = int(won.split("DataWins: ")[1].split(" BothWin:")[0])
-    bothWin = int(won.split("BothWin: ")[1].split(" MixWin:")[0])
-    mixWin = int(won.split(" MixWin: ")[1])
-    return seqWin, dataWin, bothWin, mixWin
-
 def PlotWinsByWeight(ax, i, d, weigths, distance_method, ncl):
     """Plot sequence, data, both, or mix score wins when fitting across a given set of weigths. """
     wins = []
@@ -174,3 +237,74 @@ def PlotWinsByWeight(ax, i, d, weigths, distance_method, ncl):
     X["Prioritize"] = prioritize
     X["Wins"] = wins
     sns.lineplot(x="Sequence_Weighting", y="Wins", data=X, hue="Prioritize", ax=ax)
+
+def FindWinIntegers(won):
+    """Convert wins to integers"""
+    seqWin = int(won.split("SeqWins: ")[1].split(" DataWins:")[0])
+    dataWin = int(won.split("DataWins: ")[1].split(" BothWin:")[0])
+    bothWin = int(won.split("BothWin: ")[1].split(" MixWin:")[0])
+    mixWin = int(won.split(" MixWin: ")[1])
+    return seqWin, dataWin, bothWin, mixWin
+
+def TumorType(centers):
+    """Add Normal vs Tumor column."""
+    tumortype = []
+    for i in range(centers.shape[0]):
+        if ".N" in centers["Patient_ID"][i]:
+            tumortype.append("Normal")
+        else:
+            tumortype.append("Tumor")
+    centers["Type"] = tumortype
+    return centers
+
+def TransformCPTACdataForRegression(model, d, patient_IDs):
+    """Match patient IDs to clinical outcomes for regression and return phosphoproteomic and clinical data sets."""
+    centers = model.transform(d)
+    centers["Patient_ID"] = patient_IDs
+
+    #Import Vital Status after 12 months and transform to binary
+    cf = pd.read_csv("msresist/data/MS/CPTAC/CPTACLUAD_VitalStatus.csv")
+    cf = cf.replace("Living", 0)
+    cf = cf.replace("Deceased", 1)
+
+    #Import dictionary with sample IDs to map patients in both data sets
+    IDict = pd.read_csv("msresist/data/MS/CPTAC/IDs.csv", header=0)
+    IDict_ = dict(zip(IDict.iloc[:, 0], IDict.iloc[:, 1]))
+    cf = SwapPatientIDs(cf, IDict_)
+    cf = AddTumorPerPatient(cf)
+
+    centers = centers.set_index("Patient_ID").sort_values(by="Patient_ID")
+    y = cf.sort_values(by="Patient_ID").set_index("Patient_ID")
+    
+    #Check differences in patients present in both data sets
+    diff = list(set(centers.index) - set(y.index))
+    centers = centers.drop(diff)
+    assert len(diff) < 10, "missing many samples"
+
+    #Drop patients for which there is no vital status and assert all patient IDs match
+    nans = y[np.isnan(y["vital_status_12months"])].index
+    y = y.dropna().reset_index()
+    centers = centers.drop(nans).reset_index()
+    assert all(centers.index == y.index), "samples not matching."
+    return centers, y
+
+def SwapPatientIDs(cf, IDict_):
+    """Change patient IDs from Case ID to Broad ID."""
+    ids = []
+    for i in range(cf.shape[0]):
+        ids.append(IDict_[cf.iloc[i, 0]])
+    cf["Patient_ID"] = ids
+    return cf
+
+def AddTumorPerPatient(cf):
+    """Add Tumor row per patient in vital status data."""
+    for i in range(cf.shape[0]):
+        id_ = cf.iloc[i, 0]
+        if ".N" in id_:
+            iD = id_.split(".N")[0]
+            cf.loc[-1] = [iD, cf.iloc[i, 1]]
+            cf.index = cf.index + 1
+        else:
+            continue
+    cf.index = cf.index + 1
+    return cf.sort_index()
