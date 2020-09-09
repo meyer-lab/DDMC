a49c078f5d4852a70cbdd4407b585f0e184e1a16
Marc Creixell <creixell@g.ucla.edu>
2020-09-09 16:17:04 -0700

    add ROC code. (#205)
    * add ROC code.
    
    * Update logistic_regression.py
    
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>
    Former-commit-id: e7f04097ed6971010e346b0c9362bdc39be52907 [formerly 9e5bc0f34274160dd80639534d8e4e339a49458d] [formerly a131a1701689ee0b786c42eb8b8b2c9f0cc6e8bd]
    Former-commit-id: 815d2d4c4f1951554855245fee6ca7f6cd2555f7
    Former-commit-id: df134cbc41560c35a0ca5eb7552b90ed03481814
    Former-commit-id: 55cc43b9c84c48ac1f9306c4e14a2f1e54e7684f

--- msresist/logistic_regression.py
+++ msresist/logistic_regression.py
@@ -3,8 +3,10 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from sklearn.linear_model import LogisticRegressionCV
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import auc
+from sklearn.metrics import plot_roc_curve
+from sklearn.model_selection import StratifiedKFold
 
 
 def plotClusterCoefficients(ax, lr):
@@ -41,3 +43,43 @@ def plotConfusionMatrix(ax, lr, dd, yy):
     for i in range(n):
         for j in range(n):
             ax.text(j, i, cm[i, j], ha='center', va='center', color='white')
+
+
+def plotROC(ax, classifier, d, y, cv_folds=4):
+    """Plot Receiver Operating Characteristc with cross-validation folds of a given classifier model. 
+    Note that it doesn't need to be a logistic regression."""
+    d = d.values
+    y = y.values
+    cv = StratifiedKFold(n_splits=cv_folds)
+    tprs = []
+    aucs = []
+    mean_fpr = np.linspace(0, 1, 100)
+
+    for i, (train, test) in enumerate(cv.split(d, y)):
+        classifier.fit(d[train], y[train])
+        viz = plot_roc_curve(classifier, d[test], y[test],
+                         name='ROC fold {}'.format(i),
+                         alpha=0.3, lw=1, ax=ax)
+        interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
+        interp_tpr[0] = 0.0
+        tprs.append(interp_tpr)
+        aucs.append(viz.roc_auc)
+
+    ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Chance', alpha=.8)
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+    ax.plot(mean_fpr, mean_tpr, color='b',
+            label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+            lw=2, alpha=.8)
+
+    std_tpr = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                    label=r'$\pm$ 1 std. dev.')
+
+    ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
+        title="Receiver Operating Characteristic")
+    ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0, labelspacing=0.2)
