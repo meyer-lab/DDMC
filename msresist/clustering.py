f48edde936967b1be1b93309b6a09d3bc1faa565
Aaron Meyer <ameyer@ucla.edu>
2020-09-10 20:08:56 -0700

    Linting

--- msresist/clustering.py
+++ msresist/clustering.py
@@ -4,8 +4,8 @@ import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
 from sklearn.utils.validation import check_is_fitted
-from .expectation_maximization import EM_clustering
 from Bio import motifs
+from .expectation_maximization import EM_clustering
 from .motifs import ForegroundSeqs
 from .binomial import assignPeptidesBN, position_weight_matrix, AAlist, BackgroundSeqs, GenerateBinarySeqID
 from .pam250 import assignPeptidesPAM, MotifPam250Scores
