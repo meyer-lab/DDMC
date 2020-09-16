6931f4cfbda90c05be063f20def7f9f2dab90423
Marc Creixell <creixell@g.ucla.edu>
2020-09-16 08:31:25 -0700

    Linting & Update wins and pssm (#217)
    * linting.
    
    * update .wins()
    
    * fix indices.
    
    * calculate cluster pssms.
    
    * linting.
    
    * delete figure file.
    
    * more linting.
    
    * update biopython's PAM250 matrix.
    
    * remove two cross-validation options.
    
    * Update clustering.py
    
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>

--- msresist/pam250.py
+++ msresist/pam250.py
@@ -1,11 +1,8 @@
-"""Utilization of a PAM250 transition matrix to compute sequence distance between sequences and clusters."""
-
-
 from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import shared_memory
 import numpy as np
 import scipy.stats as sp
-from Bio.SubsMat import MatrixInfo
+from Bio.Align import substitution_matrices
 
 
 def assignPeptidesPAM(ncl, scores, Seq1Seq2ToScore):
@@ -56,6 +53,8 @@ class PAM250():
 def MotifPam250Scores(seqs):
     """ Calculate and store all pairwise pam250 distances before starting """
     n = len(seqs)
+    pam250 = substitution_matrices.load("PAM250")
+    seqs = NumSeqs(seqs)
 
     # WARNING this type can only hold -128 to 127
     dtype = np.dtype(np.int8)
@@ -64,7 +63,7 @@ def MotifPam250Scores(seqs):
 
     with ProcessPoolExecutor() as e:
         for ii in range(0, n, 500):
-            e.submit(innerloop, seqs, ii, 500, shm.name, out.dtype, n)
+            e.submit(innerloop, seqs, ii, 500, shm.name, out.dtype, pam250, n)
 
         e.shutdown()
 
@@ -75,27 +74,60 @@ def MotifPam250Scores(seqs):
     i_upper = np.triu_indices(n, k=1)
     out[i_upper] = out.T[i_upper]
 
-    assert out[5, 5] == pairwise_score(seqs[5], seqs[5]), "PAM250 scores array is wrong."
+    assert out[5, 5] == pairwise_score(seqs[5], seqs[5], pam250), "PAM250 scores array is wrong."
     return out
 
 
-def innerloop(seqs, ii, endi, shm_name, ddtype, n: int):
+def innerloop(seqs, ii, endi, shm_name, ddtype, pam250, n: int):
     existing_shm = shared_memory.SharedMemory(name=shm_name)
     out = np.ndarray((n, n), dtype=ddtype, buffer=existing_shm.buf)
 
     for idxx in range(ii, ii + endi):
         for jj in range(idxx + 1):
-            out[idxx, jj] = pairwise_score(seqs[idxx], seqs[jj])
+            out[idxx, jj] = pairwise_score(seqs[idxx], seqs[jj], pam250)
 
     existing_shm.close()
 
 
-def pairwise_score(seq1: str, seq2: str) -> float:
+def pairwise_score(seq1, seq2, pam250):
     """ Compute distance between two kinase motifs. Note this does not account for gaps. """
     score = 0
     for a, b in zip(seq1, seq2):
-        if (a, b) in MatrixInfo.pam250:
-            score += MatrixInfo.pam250[(a, b)]
-        else:
-            score += MatrixInfo.pam250[(b, a)]
+        score += int(pam250[a, b])
     return score
+
+
+def NumSeqs(seqs):
+    """Transform sequences to numeric lists to access PAM250 more efficiently."""
+    numSeqs = []
+    for seq in seqs:        
+        numS = [pam250_idx[aa] for aa in seq]
+        numSeqs.append(numS)
+    return numSeqs
+
+
+pam250_idx = {
+    "A": 0,
+    "R": 1,
+    "N": 2,
+    "D": 3,
+    "C": 4,
+    "Q": 5,
+    "E": 6,
+    "G": 7,
+    "H": 8,
+    "I": 9,
+    "L": 10,
+    "K": 11,
+    "M": 12,
+    "F": 13,
+    "P": 14,
+    "S": 15,
+    "T": 16,
+    "W": 17,
+    "Y": 18,
+    "V": 19,
+    "B": 20,
+    "Z": 21,
+    "X": 22
+}
