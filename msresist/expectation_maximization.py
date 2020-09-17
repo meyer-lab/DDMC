9399e4f8e45d59a916c44f635591a239f64b8518
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-09-16 17:27:02 -0700

    Try to test wins (#221)
    * Try to test wins
    
    * Update binomial.py
    
    * Try this
    
    * Fix.
    
    * Simplify diff.
    
    * Linting
    
    * Fix overflow.
    
    * Fixed.

--- msresist/expectation_maximization.py
+++ msresist/expectation_maximization.py
@@ -1,14 +1,26 @@
 """Main Expectation-Maximization functions using gmm and binomial or pam250 to determine cluster assginments.
 EM Co-Clustering Method using a PAM250 or a Binomial Probability Matrix """
 
+from copy import deepcopy
 import numpy as np
 import scipy.stats as sp
 from pomegranate import GeneralMixtureModel, NormalDistribution, IndependentComponentsDistribution
-from .binomial import Binomial
-from .pam250 import PAM250
 
 
-def EM_clustering(data, info, ncl, SeqWeight, distance_method, background):
+def EM_clustering_repeat(nRepeats=3, *params):
+    output = EM_clustering(*params)
+
+    for _ in range(nRepeats):
+        output_temp = EM_clustering(*params)
+
+        # Use the new result if it's better
+        if output_temp[0] > output[0]:
+            output = output_temp
+
+    return output
+
+
+def EM_clustering(data, info, ncl, seqDist):
     """ Compute EM algorithm to cluster MS data using both data info and seq info.  """
     d = np.array(data.T)
 
@@ -16,23 +28,24 @@ def EM_clustering(data, info, ncl, SeqWeight, distance_method, background):
     idxx = np.atleast_2d(np.arange(d.shape[0]))
     d = np.hstack((d, idxx.T))
 
-    if distance_method == "PAM250":
-        seqDist = PAM250(info, background, SeqWeight)
-    elif distance_method == "Binomial":
-        seqDist = Binomial(info, background, SeqWeight)
+    for _ in range(2):
+        # Initialize model
+        dists = list()
+        for _ in range(ncl):
+            nDist = [NormalDistribution(sp.norm.rvs(), 0.2) for _ in range(d.shape[1] - 1)]
+            dists.append(IndependentComponentsDistribution(nDist + [deepcopy(seqDist)]))
 
-    # Initialize model
-    dists = list()
-    for _ in range(ncl):
-        nDist = [NormalDistribution(sp.norm.rvs(), 0.1) for _ in range(d.shape[1] - 1)]
-        dists.append(IndependentComponentsDistribution(nDist + [seqDist]))
+        gmm = GeneralMixtureModel(dists)
+        gmm.fit(d, inertia=0.1, stop_threshold=1e-12)
+        scores = gmm.predict_proba(d)
 
-    gmm = GeneralMixtureModel(dists)
-
-    gmm.fit(d, inertia=0.1, stop_threshold=1e-12)
-    scores = gmm.predict_proba(d)
+        if np.all(np.isfinite(scores)):
+            break
 
     seq_scores = np.exp([dd[-1].weights for dd in gmm.distributions])
     avgScore = np.sum(gmm.log_probability(d))
+
+    assert np.all(np.isfinite(scores))
+    assert np.all(np.isfinite(seq_scores))
 
     return avgScore, scores, seq_scores, gmm
--- msresist/tests/test_CoClustering.py
+++ msresist/tests/test_CoClustering.py
@@ -3,6 +3,7 @@ Testing file for the clustering methods by data and sequence.
 """
 
 import pytest
+import numpy as np
 from ..clustering import MassSpecClustering
 from ..pre_processing import preprocessing
 
@@ -12,6 +13,16 @@ data = X.select_dtypes(include=['float64']).T
 info = X.select_dtypes(include=['object'])
 
 
+@pytest.mark.parametrize("distance_method", ["PAM250", "Binomial"])
+def test_wins(distance_method):
+    """ Test that EMclustering is working by comparing with GMM clusters. """
+    MSC = MassSpecClustering(info, 2, SeqWeight=0, distance_method=distance_method).fit(X=data)
+    distances = MSC.wins(data)
+
+    # assert that the distance to the same sequence weight is less
+    assert distances[0] < 1.0
+
+
 @pytest.mark.parametrize("w", [0, 0.1, 0.3, 1])
 @pytest.mark.parametrize("ncl", [2, 3, 4])
 @pytest.mark.parametrize("distance_method", ["PAM250", "Binomial"])
@@ -19,5 +30,6 @@ def test_clusters(w, ncl, distance_method):
     """ Test that EMclustering is working by comparing with GMM clusters. """
     MSC = MassSpecClustering(info, ncl, SeqWeight=w, distance_method=distance_method).fit(X=data)
 
-    # assert that EM clusters are different than GMM clusters
-    assert True
+    # Assert that we got a reasonable result
+    assert np.all(np.isfinite(MSC.scores_))
+    assert np.all(np.isfinite(MSC.seq_scores_))
