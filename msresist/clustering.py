e8ee443fab5c89bea7d49241327292c89a77b851
Marc Creixell <creixell@g.ucla.edu>
2020-10-06 14:09:47 -0700

    add background.

--- msresist/clustering.py
+++ msresist/clustering.py
@@ -81,19 +81,26 @@ class MassSpecClustering(BaseEstimator):
     def pssms(self, bg_sequences):
         """Compute position-specific scoring matrix of each cluster."""
         pssms = []
+        back_pssm = np.zeros((len(AAlist), 11), dtype=float)
         for ii in range(self.ncl):
             pssm = np.zeros((len(AAlist), 11), dtype=float)
             for jj, seq in enumerate(self.info["Sequence"]):
                 seq = seq.upper()
                 for kk, aa in enumerate(seq):
                     pssm[AAlist.index(aa), kk] += self.scores_[jj, ii]
+                    if ii == 0:
+                        back_pssm[AAlist.index(aa), kk] += 1.0
 
             # Normalize by position across residues and remove negative outliers
-            # TODO: May be required to normalize by AAfreq within MS data motifs ––background sequences.
             for pos in range(pssm.shape[1]):
                 pssm[:, pos] /= np.mean(pssm[:, pos])
+                if ii == 0:
+                    back_pssm[:, pos] /= np.mean(back_pssm[:, pos])
             pssm = np.log2(pssm)
-            pssm[pssm == -np.inf] = 0 #-np.inf only in non-phosphorylatable aa in the central p-Site. 
+            if ii == 0:
+                back_pssm = np.log2(back_pssm)
+            pssm -= back_pssm.copy()
+            pssm = np.nan_to_num(pssm)
             pssm[pssm < -4] = -4
             pssm = pd.DataFrame(pssm)
             pssm.index = AAlist
