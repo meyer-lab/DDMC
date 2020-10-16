b71b7bc8cb24015abb4de796f469dc187a131c3e
Marc Creixell <creixell@g.ucla.edu>
2020-10-16 07:40:19 -0700

    export figure 3. (#246)
    * export figure 3.
    
    * fix.
    
    * use L2 and fix figure format.
    
    * more formatting.
    
    * save notebook.
    
    * remove PredictionProbabilities plot.
    
    * Update figureM3.py
    
    * delete notebook.
    
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>
    Former-commit-id: 2f7d7816da7602c3acd747e39ee302191e5cb348 [formerly e17d75b7107ad73e7c9674f678993c4c69a11fd4] [formerly 8c526dab0ca51f5e621b3d436bc320b8ac9ef714]
    Former-commit-id: ddaf51b65f68dcb9c377501805dc59c10d472f17
    Former-commit-id: 8c98df3e95c9a37b78eee76bc6c43abefe2e87c4
    Former-commit-id: b83bf60d83a643667e0c43ad9b7fde5f0b586668

--- msresist/figures/figureM3.py
+++ msresist/figures/figureM3.py
@@ -2,13 +2,50 @@
 This creates Figure M3.
 """
 
+import numpy as np
+import pandas as pd
+import pickle
+from scipy.stats import zscore
+from sklearn.linear_model import LogisticRegressionCV
 from .common import subplotLabel, getSetup
+from ..figures.figureM2 import TumorType
+from ..logistic_regression import plotClusterCoefficients, plotPredictionProbabilities, plotConfusionMatrix, plotROC
+from ..figures.figure3 import plotPCA
 
 
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((10, 10), (1, 1))
+    ax, f = getSetup((15, 10), (2, 3))
+
+    X = pd.read_csv("msresist/data/MS/CPTAC/CPTAC-preprocessedMotfis.csv").iloc[:, 1:]
+
+    with open('CPTACmodel_PAM250_W1_15CL', 'rb') as p:
+        model = pickle.load(p)[0]
+
+    centers = pd.DataFrame(model.transform())
+    centers["Patient_ID"] = X.columns[4:]
+    centers.iloc[:, :-1] = zscore(centers.iloc[:, :-1], axis=1)
+    centers.columns = list(np.arange(model.ncl) + 1) + ["Patient_ID"]
+
+    #first plot heatmap of clusters
+    ax[0].axis("off")
+
+    #PCA analysis
+    centers = TumorType(centers)
+    plotPCA(ax[1:3], centers, 2, ["Patient_ID", "Type"], "Cluster", hue_scores="Type", style_scores="Type", hue_load="Cluster")
+
+    #Regression
+    c = centers.select_dtypes(include=['float64'])
+    tt = centers.iloc[:, -1]
+    tt = tt.replace("Normal", 0)
+    tt = tt.replace("Tumor", 1)
+    lr = LogisticRegressionCV(cv=model.ncl, solver="saga", max_iter=10000, n_jobs=-1, penalty="elasticnet", class_weight="balanced", l1_ratios=[0.5, 0.9]).fit(c, tt)
+
+    # plotPredictionProbabilities(ax[3], lr, c, tt)
+    plotConfusionMatrix(ax[3], lr, c, tt)
+    plotROC(ax[4], lr, c, tt, cv_folds=model.ncl)
+    plotClusterCoefficients(ax[5], lr)
 
     # Add subplot labels
     subplotLabel(ax)
