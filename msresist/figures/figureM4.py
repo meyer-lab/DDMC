5bd4e878c1fe3c689e6f6ce062583b9c67cb5860
Marc Creixell <creixell@g.ucla.edu>
2020-11-02 14:00:31 -0800

    Fig m5 (#261)
    * fig5 regression attempts.
    
    * increase number of clusters and update figure5.
    
    * add fig5 on makefile
    
    * change number of CV fold to 4.
    
    Co-authored-by: marcc <creixell@ucla.edu>
    Former-commit-id: 189151a6ce3f06182be31dcaddf8278da31f7290 [formerly abc381c1ed92eb8ef565a59dd85a739cbab2ed65] [formerly a96bae27c2aae0b5b8d2bcd2b01d8710bfc83beb]
    Former-commit-id: a5239862a4991c38ca68f107bfc4e416f4be2ba7
    Former-commit-id: 19a13b3b65c5878d3062696b48bd761919e1966f
    Former-commit-id: c0d380b792bd3265d630899e7e84c3d4a7b7a2bb

--- msresist/figures/figureM4.py
+++ msresist/figures/figureM4.py
@@ -11,14 +11,13 @@ from sklearn.linear_model import LogisticRegressionCV
 from ..pre_processing import filter_NaNpeptides
 from ..logistic_regression import plotClusterCoefficients, plotPredictionProbabilities, plotConfusionMatrix, plotROC
 from sklearn.cross_decomposition import PLSRegression
-
 from .common import subplotLabel, getSetup
 
 
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((10, 10), (3, 3))
+    ax, f = getSetup((15, 12), (2, 3))
 
     # Add subplot labels
     subplotLabel(ax)
@@ -27,9 +26,8 @@ def makeFigure():
     sns.set(style="whitegrid", font_scale=1.2, color_codes=True, palette="colorblind", rc={"grid.linestyle": "dotted", "axes.linewidth": 0.6})
 
     # Load Clustering Model from Figure 2
-    ncl = 15
-    with open('msresist/data/pickled_models/CPTACmodel_PAM250_W1_15CL', 'rb') as ff:
-        MSC = pickle.load(ff)[0]
+    with open('msresist/data/pickled_models/CPTACmodel_PAM250_21CL_W3_TMT2', 'rb') as p:
+        model = pickle.load(p)[0]
 
     # Regression against mutation status of driver genes and clusters
     # Import mutation status of TP53, KRAS, EGFR, and ALK fusion
@@ -38,31 +36,34 @@ def makeFigure():
     mOI = mOI[~mOI["Sample.ID"].str.contains("IR")]
 
     # #### Import clusters
-    centers = MSC.transform()
+    centers = model.transform()
     y = mOI.set_index("Sample.ID")
 
     # Logistic Regression
     # EGFR mutation status
     y_egfr = y["EGFR.mutation.status"]
 
-    lr = LogisticRegressionCV(cv=ncl, solver="saga", max_iter=10000, n_jobs=-1, penalty="elasticnet", class_weight="balanced", l1_ratios=[0.5, 0.9])
+    lr = LogisticRegressionCV(cv=model.ncl, solver="saga", max_iter=10000, n_jobs=-1, penalty="elasticnet", class_weight="balanced", l1_ratios=[0.5, 0.9])
 
-    lr.fit(centers, y_egfr)
+    lr_egfr = lr.fit(centers, y_egfr)
 
-    plotPredictionProbabilities(ax[1], lr, centers, y_egfr)
-    plotConfusionMatrix(ax[2], lr, centers, y_egfr)
-    plotROC(ax[3], lr, centers, y_egfr, cv_folds=ncl)
-    plotClusterCoefficients(ax[4], lr)
+    plotConfusionMatrix(ax[0], lr_egfr, centers, y_egfr)
+    ax[0].set_title("EGFR confusion matrix")
+    plotROC(ax[1], lr_egfr, centers, y_egfr, cv_folds=4)
+    ax[1].set_title("EGFR ROC")
+    plotClusterCoefficients(ax[2], lr_egfr)
+    ax[2].set_title("EGFR Cluster Coefficients")
 
     # TP53 mutation status
     y_tp53 = y["TP53.mutation.status"]
 
-    lr.fit(centers, y_tp53)
-    y_pred = lr.predict(centers)
+    lr_tp53 = lr.fit(centers, y_tp53)
 
-    plotPredictionProbabilities(ax[5], lr, centers, y_tp53)
-    plotConfusionMatrix(ax[6], lr, centers, y_tp53)
-    plotROC(ax[7], lr, centers, y_tp53, cv_folds=ncl)
-    plotClusterCoefficients(ax[8], lr)
+    plotConfusionMatrix(ax[3], lr_tp53, centers, y_tp53)
+    ax[3].set_title("TP53 confusion matrix")
+    plotROC(ax[4], lr_tp53, centers, y_tp53, cv_folds=4)
+    ax[4].set_title("TP53 ROC")
+    plotClusterCoefficients(ax[5], lr_tp53)
+    ax[5].set_title("TP53 Cluster Coefficients")
 
     return f
