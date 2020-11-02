59e5cf620dbfc9cd9d07c131e217fd75b48b0ff5
Marc Creixell <creixell@g.ucla.edu>
2020-11-02 14:00:31 -0800

    Fig m5 (#261)
    * fig5 regression attempts.
    
    * increase number of clusters and update figure5.
    
    * add fig5 on makefile
    
    * change number of CV fold to 4.
    
    Co-authored-by: marcc <creixell@ucla.edu>
    Former-commit-id: 189151a6ce3f06182be31dcaddf8278da31f7290 [formerly abc381c1ed92eb8ef565a59dd85a739cbab2ed65] [formerly 19a13b3b65c5878d3062696b48bd761919e1966f [formerly a5239862a4991c38ca68f107bfc4e416f4be2ba7] [formerly a96bae27c2aae0b5b8d2bcd2b01d8710bfc83beb]]
    Former-commit-id: 19a13b3b65c5878d3062696b48bd761919e1966f [formerly a5239862a4991c38ca68f107bfc4e416f4be2ba7]
    Former-commit-id: 19a13b3b65c5878d3062696b48bd761919e1966f
    Former-commit-id: c0d380b792bd3265d630899e7e84c3d4a7b7a2bb
    Former-commit-id: 5bd4e878c1fe3c689e6f6ce062583b9c67cb5860
    Former-commit-id: 5bbcd3e1556a3348277abdaa9a2e15b01080fd3d

---
+++ msresist/figures/figureM5.py
@@ -0,0 +1,75 @@
+"""
+This creates Figure 5.
+"""
+import pickle
+import numpy as np
+import pandas as pd
+from ..pre_processing import filter_NaNpeptides
+from ..figures.figureM2 import SwapPatientIDs, AddTumorPerPatient
+from .common import subplotLabel, getSetup
+from ..logistic_regression import plotROC, plotConfusionMatrix, plotClusterCoefficients
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.svm import LinearSVC
+
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure"""
+    # Get list of axis objects
+    ax, f = getSetup((15, 10), (2, 3))
+
+    with open('msresist/data/pickled_models/CPTACmodel_PAM250_21CL_W3_TMT2', 'rb') as m:
+        model = pickle.load(m)[0]
+
+    # Import cancer stage data
+    cd = pd.read_csv("msresist/data/MS/CPTAC/CPTAC_Clinical_Data_May2019.csv")
+    ts = cd[["case_id", "tumor_stage_pathological"]]
+    IDict = pd.read_csv("msresist/data/MS/CPTAC/IDs.csv", header=0)
+    IDict_ = dict(zip(IDict.iloc[:, 0], IDict.iloc[:, 1]))
+    ts = SwapPatientIDs(ts, IDict_).drop("case_id", axis=1)[["Patient_ID", "tumor_stage_pathological"]]
+    ts = AddTumorPerPatient(ts).sort_values(by="Patient_ID")
+    ts = ts.replace("Stage I", 0)
+    ts = ts.replace("Stage IA", 0)
+    ts = ts.replace("Stage IB", 0)
+    ts = ts.replace("Stage IIA", 0)
+    ts = ts.replace("Stage IIB", 1)
+    ts = ts.replace("Stage III", 1)
+    ts = ts.replace("Stage IIIA", 1)
+    ts = ts.replace("Stage IV", 1)
+
+    # ts = ts.replace("Stage I", 0)
+    # ts = ts.replace("Stage IA", 1)
+    # ts = ts.replace("Stage IB", 2)
+    # ts = ts.replace("Stage IIA", 3)
+    # ts = ts.replace("Stage IIB", 4)
+    # ts = ts.replace("Stage III", 5)
+    # ts = ts.replace("Stage IIIA", 6)
+    # ts = ts.replace("Stage IV", 7)
+
+    # Find cluster centers
+    centers = pd.DataFrame(model.transform())
+    X = filter_NaNpeptides(pd.read_csv("msresist/data/MS/CPTAC/CPTAC-preprocessedMotfis.csv").iloc[:, 1:], cut=0.1)
+    centers["Patient_ID"] = X.columns[4:]
+    centers = centers.sort_values(by="Patient_ID").set_index("Patient_ID").drop(['C3N.02379.1', 'C3N.02587', 'C3N.02587.N']).reset_index()
+    assert list(ts["Patient_ID"]) == list(centers["Patient_ID"]), "Patients don't match"
+
+    x, y = np.array(centers.iloc[:, 1:]), np.array(ts.iloc[:, 1])
+    y = pd.DataFrame(y)
+
+    # Run Logistic Regression
+    lr = LogisticRegressionCV(cv=model.ncl, solver="saga", max_iter=10000, n_jobs=-1, penalty="elasticnet", class_weight="balanced", l1_ratios=[0.5, 0.7]).fit(x, y)
+    plotConfusionMatrix(ax[0], lr, x, y)
+    ax[0].set_title("LR Confusion Matrix")
+    plotROC(ax[1], lr, x, y, cv_folds=4)
+    ax[1].set_title("LR ROC")
+    plotClusterCoefficients(ax[2], lr)
+
+    # Run SVC
+    clf = LinearSVC(penalty="l1", dual=False, class_weight="balanced").fit(x, y)
+    plotConfusionMatrix(ax[3], clf, x, y)
+    ax[3].set_title("SVC Confusion Matrix")
+    plotROC(ax[4], clf, x, pd.DataFrame(y), cv_folds=4)
+    ax[4].set_title("SVC ROC")
+    plotClusterCoefficients(ax[5], clf)
+    ax[5].set_title("SVC Cluster Coefficients")
+
+    return f
