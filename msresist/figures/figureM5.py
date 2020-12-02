029e5db3a9730605017c101f5d61a918a6e99dfa
Marc Creixell <creixell@g.ucla.edu>
2020-12-02 12:10:38 -0800

    figM5 kuskal testing.

--- msresist/figures/figureM5.py
+++ msresist/figures/figureM5.py
@@ -4,18 +4,18 @@ This creates Figure 5.
 import pickle
 import numpy as np
 import pandas as pd
+import seaborn as sns
+from scipy.stats import kruskal
+from .common import subplotLabel, getSetup
 from ..pre_processing import filter_NaNpeptides
 from ..figures.figureM2 import SwapPatientIDs, AddTumorPerPatient
-from .common import subplotLabel, getSetup
-from ..logistic_regression import plotROC, plotConfusionMatrix, plotClusterCoefficients
-from sklearn.linear_model import LogisticRegressionCV
-from sklearn.svm import LinearSVC
+from ..figures.figure3 import plotPCA, plotMotifs, plotUpstreamKinases
 
 
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((15, 10), (2, 3))
+    ax, f = getSetup((12, 10), (3, 2), multz={0: 1})
 
     with open('msresist/data/pickled_models/binomial/CPTACmodel_BINOMIAL_CL24_W15_TMT2', 'rb') as p:
         model = pickle.load(p)[0]
@@ -27,23 +27,15 @@ def makeFigure():
     IDict_ = dict(zip(IDict.iloc[:, 0], IDict.iloc[:, 1]))
     ts = SwapPatientIDs(ts, IDict_).drop("case_id", axis=1)[["Patient_ID", "tumor_stage_pathological"]]
     ts = AddTumorPerPatient(ts).sort_values(by="Patient_ID")
-    ts = ts.replace("Stage I", 0)
-    ts = ts.replace("Stage IA", 0)
-    ts = ts.replace("Stage IB", 0)
-    ts = ts.replace("Stage IIA", 0)
-    ts = ts.replace("Stage IIB", 1)
-    ts = ts.replace("Stage III", 1)
-    ts = ts.replace("Stage IIIA", 1)
-    ts = ts.replace("Stage IV", 1)
 
-    # ts = ts.replace("Stage I", 0)
-    # ts = ts.replace("Stage IA", 1)
-    # ts = ts.replace("Stage IB", 2)
-    # ts = ts.replace("Stage IIA", 3)
-    # ts = ts.replace("Stage IIB", 4)
-    # ts = ts.replace("Stage III", 5)
-    # ts = ts.replace("Stage IIIA", 6)
-    # ts = ts.replace("Stage IV", 7)
+    ts = ts.replace("Stage I", 0)
+    ts = ts.replace("Stage IA", 1)
+    ts = ts.replace("Stage IB", 2)
+    ts = ts.replace("Stage IIA", 3)
+    ts = ts.replace("Stage IIB", 4)
+    ts = ts.replace("Stage III", 5)
+    ts = ts.replace("Stage IIIA", 6)
+    ts = ts.replace("Stage IV", 7)
 
     # Find cluster centers
     centers = pd.DataFrame(model.transform())
@@ -52,24 +44,42 @@ def makeFigure():
     centers = centers.sort_values(by="Patient_ID").set_index("Patient_ID").drop(['C3N.02379.1', 'C3N.02587', 'C3N.02587.N']).reset_index()
     assert list(ts["Patient_ID"]) == list(centers["Patient_ID"]), "Patients don't match"
 
-    x, y = np.array(centers.iloc[:, 1:]), np.array(ts.iloc[:, 1])
-    y = pd.DataFrame(y)
+    # Run Kruskal-Wallis H-test
+    plot_ClinicalStaging_Kuskal(centers, ts, model, ax=ax[0])
 
-    # Run Logistic Regression
-    lr = LogisticRegressionCV(cv=model.ncl, solver="saga", max_iter=10000, n_jobs=-1, penalty="elasticnet", class_weight="balanced", l1_ratios=[0.5, 0.7]).fit(x, y)
-    plotConfusionMatrix(ax[0], lr, x, y)
-    ax[0].set_title("LR Confusion Matrix")
-    plotROC(ax[1], lr, x, y, cv_folds=4)
-    ax[1].set_title("LR ROC")
-    plotClusterCoefficients(ax[2], lr)
-
-    # Run SVC
-    clf = LinearSVC(penalty="l1", dual=False, class_weight="balanced").fit(x, y)
-    plotConfusionMatrix(ax[3], clf, x, y)
-    ax[3].set_title("SVC Confusion Matrix")
-    plotROC(ax[4], clf, x, pd.DataFrame(y), cv_folds=4)
-    ax[4].set_title("SVC ROC")
-    plotClusterCoefficients(ax[5], clf)
-    ax[5].set_title("SVC Cluster Coefficients")
+    # Motifs and kinase predictions
+    pssms = model.pssms(PsP_background=False)
+    motifs = [pssms[14], pssms[17]]
+    plotMotifs(motifs, [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5], titles=["Cluster 15", "Cluster 18"], axes=ax[1:3])
+    plotUpstreamKinases(model, ax=ax[3:5], clusters_=[15, 18], n_components=4, pX=1)
 
     return f
+
+
+def plot_ClinicalStaging_Kuskal(centers, ts, model, ax):
+    centers["Stage"] = np.array(ts.iloc[:, 1])
+    centers_ = centers.iloc[:, 1:].set_index("Stage")
+    cluster_samples = []
+    for ii in range(model.ncl):
+        samples = []
+        for jj in range(max(centers_.index)):
+            samples.append(centers_.iloc[:, ii].loc[jj].values)
+        cluster_samples.append(samples)
+    pvals = []
+    for c in cluster_samples:
+        s1, s2, s3, s4, s5, s6, s7 = c
+        pval = kruskal(s1, s2, s3, s4, s5, s6, s7)[1]
+        pvals.append(pval)
+
+    #plot pvalues
+    data = pd.DataFrame()
+    data["Clusters"] = np.arange(model.ncl)
+    data["Kuskal p-value"] = pvals
+    signif = []
+    for val in pvals:
+        if val < 0.05:
+            signif.append(True)
+        else:
+            signif.append(False)
+    data["Significant"] = signif
+    sns.barplot(x="Clusters", y="Kuskal p-value", hue="Significant", data=data, ax=ax)
