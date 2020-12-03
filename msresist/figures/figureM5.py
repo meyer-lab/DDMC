90b5e44c45846dbeaa4e1cb65ef1225cf16cc0a5
Marc Creixell <creixell@g.ucla.edu>
2020-12-03 10:32:04 -0800

    group stages.

--- msresist/figures/figureM5.py
+++ msresist/figures/figureM5.py
@@ -15,7 +15,7 @@ from ..figures.figure3 import plotPCA, plotMotifs, plotUpstreamKinases
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((15, 9), (2, 4), multz={0:1})
+    ax, f = getSetup((12, 12), (2, 2))
 
     with open('msresist/data/pickled_models/binomial/CPTACmodel_BINOMIAL_CL24_W15_TMT2', 'rb') as p:
         model = pickle.load(p)[0]
@@ -30,12 +30,12 @@ def makeFigure():
 
     ts = ts.replace("Stage I", 0)
     ts = ts.replace("Stage IA", 0)
-    ts = ts.replace("Stage IB", 1)
-    ts = ts.replace("Stage IIA", 2)
-    ts = ts.replace("Stage IIB", 3)
-    ts = ts.replace("Stage III", 4)
-    ts = ts.replace("Stage IIIA", 4)
-    ts = ts.replace("Stage IV", 4)
+    ts = ts.replace("Stage IB", 0)
+    ts = ts.replace("Stage IIA", 1)
+    ts = ts.replace("Stage IIB", 1)
+    ts = ts.replace("Stage III", 2)
+    ts = ts.replace("Stage IIIA", 2)
+    ts = ts.replace("Stage IV", 2)
 
     # Find cluster centers
     centers = pd.DataFrame(model.transform())
@@ -48,14 +48,13 @@ def makeFigure():
     plot_ClinicalStaging_Kuskal(centers, ts, model, ax=ax[0])
 
     # Plot p-site abundance per stage
-    plot_abundanceVSstage(centers, 16, ts, ax=ax[1])
-    plot_abundanceVSstage(centers, 19, ts, ax=ax[2])
+    plot_abundanceVSstage(centers, 7, ts, ax=ax[1])
 
     # Motifs and kinase predictions
-    pssms = model.pssms(PsP_background=True)
-    motifs = [pssms[15], pssms[18]]
-    plotMotifs(motifs, [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5], titles=["Cluster 16", "Cluster 19"], axes=ax[3:5])
-    plotUpstreamKinases(model, ax=ax[5:7], clusters_=[16, 19], n_components=4, pX=1)
+    pssms = model.pssms(PsP_background=False)
+    motifs = [pssms[6]]
+    plotMotifs(motifs, [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5], titles=["Cluster 7"], axes=[ax[2]])
+    plotUpstreamKinases(model, ax=ax[3], clusters_=[7], n_components=2, labels=False, pX=1)
 
     return f
 
@@ -67,13 +66,13 @@ def plot_ClinicalStaging_Kuskal(centers, ts, model, ax):
     cluster_samples = []
     for ii in range(model.ncl):
         samples = []
-        for jj in range(max(centers_.index)):
+        for jj in range(max(centers_.index) + 1):
             samples.append(centers_.iloc[:, ii].loc[jj].values)
         cluster_samples.append(samples)
     pvals = []
     for c in cluster_samples:
-        s1, s2, s3, s4 = c
-        pval = kruskal(s1, s2, s3, s4)[1]
+        s1, s2, s3 = c
+        pval = kruskal(s1, s2, s3)[1]
         pvals.append(pval)
 
     #plot pvalues
@@ -82,7 +81,7 @@ def plot_ClinicalStaging_Kuskal(centers, ts, model, ax):
     data["p-value"] = pvals
     signif = []
     for val in pvals:
-        if val < 0.05:
+        if val < 0.06:
             signif.append(True)
         else:
             signif.append(False)
@@ -98,7 +97,7 @@ def plot_abundanceVSstage(centers, cluster, ts, ax):
     X = centers.loc[:, ["Patient_ID", "Stage", cluster]]
     sns.stripplot(x="Stage", y=cluster, data=X, color="darkblue", ax=ax)
     sns.boxplot(x="Stage", y=cluster, data=X, color="white", linewidth=2, ax=ax)
-    ax.set_xticklabels(["IA", "IB", "IIA", "IIB", "III+"])
+    ax.set_xticklabels(["I", "II", "III"])
     ax.set_title("p-site Abundance")
     ax.set_ylabel("Cluster " + str(cluster + 1))
 
