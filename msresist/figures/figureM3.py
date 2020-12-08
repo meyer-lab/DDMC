91900a1c22978534b6d7f5782244346255d292d5
Marc Creixell <creixell@g.ucla.edu>
2020-12-07 19:09:54 -0800

    hypothesis testing figureM4.svg

--- msresist/figures/figureM3.py
+++ msresist/figures/figureM3.py
@@ -32,7 +32,6 @@ def makeFigure():
 
     centers = pd.DataFrame(model.transform())
     centers["Patient_ID"] = X.columns[4:]
-    centers.iloc[:, :-1] = zscore(centers.iloc[:, :-1], axis=1)
     centers.columns = list(np.arange(model.ncl) + 1) + ["Patient_ID"]
 
     # first plot heatmap of clusters
@@ -40,14 +39,16 @@ def makeFigure():
 
     # PCA analysis
     centers = TumorType(centers)
-    pvals = build_pval_matrix(calculate_mannW_pvals(centers), 24).iloc[:, -1].values
-    plotPCA(ax[1:3], centers, 2, ["Patient_ID", "Type"], "Cluster", hue_scores="Type", style_scores="Type", pvals=pvals)
+    pvals = build_pval_matrix(calculate_mannW_pvals(centers, "Type", "Normal", "Tumor"), 24).iloc[:, -1].values
+    c_ = centers.copy()
+    c_.iloc[:, :-2] = zscore(c_.iloc[:, :-2], axis=1) #zscore for PCA 
+    plotPCA(ax[1:3], c_, 2, ["Patient_ID", "Type"], "Cluster", hue_scores="Type", style_scores="Type", pvals=pvals)
 
     # Plot NAT vs tumor signal per cluster
     plot_clusters_binaryfeatures(centers, "Type", ax[3])
 
     # Regression
-    c = centers.select_dtypes(include=['float64'])
+    c = c_.select_dtypes(include=['float64'])
     tt = centers.iloc[:, -1]
     tt = tt.replace("Normal", 0)
     tt = tt.replace("Tumor", 1)
@@ -58,12 +59,12 @@ def makeFigure():
     plotClusterCoefficients(ax[6], lr)
 
     # plot Cluster Motifs
-    pssms = model.pssms(PsP_background=True)
-    motifs = [pssms[9], pssms[10]]
-    plotMotifs(motifs, [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5], titles=["Cluster 10", "Cluster 11"], axes=ax[7:9])
+    pssms = model.pssms(PsP_background=False)
+    motifs = [pssms[10], pssms[11]]
+    plotMotifs(motifs, [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5], titles=["Cluster 11", "Cluster 12"], axes=ax[7:9])
 
     # plot Upstream Kinases
-    plotUpstreamKinases(model, ax=ax[9:11], clusters_=[10, 11], n_components=4, pX=1)
+    plotUpstreamKinases(model, ax=ax[9:11], clusters_=[11, 12], n_components=4, pX=1)
 
     # Add subplot labels
     subplotLabel(ax)
@@ -75,17 +76,17 @@ def plot_clusters_binaryfeatures(centers, id_var, ax):
     """Plot p-signal of binary features (tumor vs NAT or mutational status) per cluster """
     ncl = centers.shape[1] - 2
     data = pd.melt(id_vars=id_var, value_vars=np.arange(ncl)+1, value_name="p-signal", var_name="Cluster", frame=centers)
-    sns.stripplot(x="Cluster", y="p-signal", hue="Type", data=data, dodge=True, ax=ax)
+    sns.stripplot(x="Cluster", y="p-signal", hue=id_var, data=data, dodge=True, ax=ax)
 
 
-def calculate_mannW_pvals(centers):
+def calculate_mannW_pvals(centers, col, feature1, feature2):
     """Compute Mann Whitney rank test p-values"""
     pvals = []
     for ii in range(centers.shape[1] - 2):
         x = centers.iloc[:, [ii, -1]]
-        x_t = x[x["Type"] == "Normal"].iloc[:, 0]
-        x_nat = x[x["Type"] == "Tumor"].iloc[:, 0]
-        pval = mannwhitneyu(x_t, x_nat)[1]
+        x1 = x[x[col] == feature1].iloc[:, 0]
+        x2 = x[x[col] == feature2].iloc[:, 0]
+        pval = mannwhitneyu(x1, x2)[1]
         pvals.append(pval)
     return pvals
 
@@ -105,6 +106,3 @@ def build_pval_matrix(pvals, ncl):
             signif.append("Not Significant")
     data["Significant"] = signif
     return data
-
-
-
