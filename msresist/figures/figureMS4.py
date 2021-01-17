fdee2c66a109ba6a7d155dce589a2db32f51e2a0
Marc Creixell <creixell@g.ucla.edu>
2021-01-16 17:28:22 -0800

    fix logistic regression problem. (#321)
    * fix logistic regression problem.
    
    * .
    
    * update EGFR/ALK and STK11 predictions.
    
    * fix figM5.svg
    
    * tweak MultiTaskLasso
    
    * STK11m main, EGFRm supplemental.
    
    * delete files.
    
    * import functions.
    
    * fix.
    
    * update S4.
    
    * switch to SVC instead of LR.
    
    * switch to SVC in figureMS5
    
    * fix figureMS4.svg
    
    * fix.
    
    * figM3 LR.
    
    Co-authored-by: marcc <creixell@ucla.edu>
    Former-commit-id: 56a68be7cd67f9730ac3528393bc80a4979be36f [formerly 7ae17db042a862249298b78179f08afb45fc495e] [formerly e5de57d9970159d8cb59fc529b09129b3ae49a85 [formerly ea5f20f77c758c4f39c71ba3d37ef7ec7e907817] [formerly 2714a8f53013b424647a2d00b6781cc551d06125]]
    Former-commit-id: e5de57d9970159d8cb59fc529b09129b3ae49a85 [formerly ea5f20f77c758c4f39c71ba3d37ef7ec7e907817]
    Former-commit-id: e5de57d9970159d8cb59fc529b09129b3ae49a85
    Former-commit-id: 7bdb244c5b0446f6ae62c5ac01de7e9facc4b09b
    Former-commit-id: f51305b8d2fc2783aeb973e2f8f540a6cfa3681d
    Former-commit-id: b4d14cb9398d5f16b6333aaa8433419351e0a44f

--- msresist/figures/figureMS4.py
+++ msresist/figures/figureMS4.py
@@ -12,13 +12,14 @@ from .common import subplotLabel, getSetup
 from ..pre_processing import filter_NaNpeptides
 from ..logistic_regression import plotClusterCoefficients, plotConfusionMatrix, plotROC
 from .figureM3 import plot_clusters_binaryfeatures, build_pval_matrix, calculate_mannW_pvals
+from .figureM4 import find_patients_with_NATandTumor
 from .figureMS3 import plot_unclustered_LRcoef
 
 
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((10, 17), (5, 2), multz={4: 1, 8: 1})
+    ax, f = getSetup((9, 20), (5, 2), multz={4: 1, 8: 1})
 
     # Set plotting format
     sns.set(style="whitegrid", font_scale=1.2, color_codes=True, palette="colorblind", rc={"grid.linestyle": "dotted", "axes.linewidth": 0.6})
@@ -26,62 +27,70 @@ def makeFigure():
     # Add subplot labels
     subplotLabel(ax)
 
-    # TP53 WT vs mut unclustered
+    # STK11 WT vs mut unclustered
     X = pd.read_csv("msresist/data/MS/CPTAC/CPTAC-preprocessedMotfis.csv").iloc[:, 1:]
     X = filter_NaNpeptides(X, cut=1)
     d = X.set_index("Gene").select_dtypes(include=["float64"]).T.reset_index()
     d.rename(columns={"index": "Patient_ID"}, inplace=True)
+    d = d.iloc[:, 1:]
 
     mutations = pd.read_csv("msresist/data/MS/CPTAC/Patient_Mutations.csv")
     mOI = mutations[["Sample.ID"] + list(mutations.columns)[45:54] + list(mutations.columns)[61:64]]
-    mOI = mOI[~mOI["Sample.ID"].str.contains("IR")]
-    y = mOI.set_index("Sample.ID")
-    y = y["TP53.mutation.status"]
+    y = mOI[~mOI["Sample.ID"].str.contains("IR")]
+    y_ = y["STK11.mutation.status"]
 
-    # Remove NATs
-    X = X.loc[:, ~X.columns.str.endswith(".N")]
-    d = d[~d["Patient_ID"].str.endswith(".N")].iloc[:, 1:]
-    y = y[~y.index.str.endswith(".N")]
-    z = d.copy()
-    z["TP53 status"] = y.values
+    lr = LogisticRegressionCV(Cs=10, cv=15, solver="saga", max_iter=10000, n_jobs=-1, penalty="l1", class_weight="balanced")
 
-    lr = LogisticRegressionCV(cv=4, solver="saga", max_iter=10000, n_jobs=-1, penalty="l1", class_weight="balanced")
-    uc_lr = lr.fit(d, y)
+    plotROC(ax[0], lr, d.values, y_, cv_folds=4, title="ROC unclustered")
+    plot_unclustered_LRcoef(ax[1], lr.fit(d, y_), d)
 
-    plotROC(ax[0], uc_lr, d.values, y, cv_folds=4, title="ROC unclustered")
-    plot_unclustered_LRcoef(ax[1], uc_lr, z)
-
-    # TP53 WT vs mut k-means
-    ncl = 15
-    labels = KMeans(n_clusters=ncl).fit(d.T).labels_
+    # Run k-means
+    ncl = 24
     x_ = X.copy()
-    x_["Cluster"] = labels
+    x_["Cluster"] = KMeans(n_clusters=ncl).fit(d.T).labels_
     c_kmeans = x_.groupby("Cluster").mean().T
-    c_kmeans.columns = list(np.arange(ncl) + 1)
-    km_lr = lr.fit(c_kmeans, y)
-    plotROC(ax[2], km_lr, c_kmeans.values, y, cv_folds=4, title="ROC k-means")
-    plotClusterCoefficients(ax[3], lr, "k-means")
-    c_kmeans["TP53 status"] = z.iloc[:, -1].values
-    pvals = calculate_mannW_pvals(c_kmeans, "TP53 status", 0, 1)
-    pvals = build_pval_matrix(ncl, pvals)
-    plot_clusters_binaryfeatures(c_kmeans, "TP53 status", ax[4], pvals=pvals)
+    c_kmeans["Patient_ID"] = X.columns[4:]
+    c_kmeans.columns = list(np.arange(ncl) + 1) + ["Patient_ID"]
 
-    # Tumor vs NAT GMM
+    # Reshape data (Patients vs NAT and tumor sample per cluster)
+    c_kmeansT = find_patients_with_NATandTumor(c_kmeans.copy(), "Patient_ID", conc=True)
+    yT = find_patients_with_NATandTumor(y.copy(), "Sample.ID", conc=False)
+    assert all(c_kmeansT.index.values == yT.index.values), "Samples don't match"
+
+    # Regress k-means clsuters against STK11 status
+    c_kmeans["STK11"] = y["STK11.mutation.status"].values
+    c_kmeansT["STK11"] = yT["STK11.mutation.status"].values
+    c_kmeans = c_kmeans.set_index("Patient_ID")
+    plotROC(ax[2], lr, c_kmeansT.iloc[:, :-1].values, c_kmeansT["STK11"], cv_folds=4, title="ROC k-means")
+    plotClusterCoefficients(ax[3], lr.fit(c_kmeansT.iloc[:, :-1], c_kmeansT["STK11"].values), title="k-means")
+    pvals = calculate_mannW_pvals(c_kmeans, "STK11", 0, 1)
+    pvals = build_pval_matrix(ncl, pvals)
+    plot_clusters_binaryfeatures(c_kmeans, "STK11", ax[4], pvals=pvals)
+
+    # Run GMM
+    ncl = 15
     for _ in range(10):
         gmm = GeneralMixtureModel.from_samples(NormalDistribution, X=d.T, n_components=ncl, n_jobs=-1)
         scores = gmm.predict_proba(d.T)
         if np.all(np.isfinite(scores)):
             break
-    x_ = X.copy()
     x_["Cluster"] = gmm.predict(d.T)
     c_gmm = x_.groupby("Cluster").mean().T
-    c_gmm.columns = list(np.arange(ncl) + 1)
-    gmm_lr = lr.fit(c_gmm, y)
-    plotROC(ax[5], gmm_lr, c_gmm.values, y, cv_folds=4, title="ROC GMM")
-    plotClusterCoefficients(ax[6], gmm_lr, title="GMM")
-    c_gmm["TP53 status"] = z.iloc[:, -1].values
-    pvals = calculate_mannW_pvals(c_gmm, "TP53 status", 0, 1)
+    c_gmm["Patient_ID"] = X.columns[4:]
+    c_gmm.columns = list(np.arange(ncl) + 1) + ["Patient_ID"]
+
+    # Reshape data (Patients vs NAT and tumor sample per cluster)
+    c_gmmT = find_patients_with_NATandTumor(c_gmm.copy(), "Patient_ID", conc=True)
+    assert all(c_gmmT.index.values == yT.index.values), "Samples don't match"
+
+    # Regress GMM clusters against STK11 status
+    c_gmm["STK11"] = y["STK11.mutation.status"].values
+    c_gmmT["STK11"] = yT["STK11.mutation.status"].values
+    c_gmm = c_gmm.set_index("Patient_ID")
+    plotROC(ax[5], lr, c_gmmT.iloc[:, :-1].values, c_gmmT["STK11"], cv_folds=4, title="ROC GMM")
+    plotClusterCoefficients(ax[6], lr.fit(c_gmmT.iloc[:, :-1], c_gmmT["STK11"].values), title="GMM")
+    pvals = calculate_mannW_pvals(c_gmm, "STK11", 0, 1)
     pvals = build_pval_matrix(ncl, pvals)
-    plot_clusters_binaryfeatures(c_gmm, "TP53 status", ax[7], pvals=pvals)
+    plot_clusters_binaryfeatures(c_gmm, "STK11", ax[7], pvals=pvals)
 
     return f
