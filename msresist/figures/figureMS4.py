df235c05fcfb22b6ad2702810a9ae0f4d791e9b5
Marc Creixell <creixell@g.ucla.edu>
2021-02-09 17:19:00 +0100

    Update Results Text (#334)
    * update text figureM3.
    
    * text edits and add comparison of different strategies in figureMS5.svg.
    
    * text edits.
    
    * update makefile
    
    * again.
    
    * update text figure3
    
    * add PsP K-S file.
    
    * delete figure files.
    
    * update text.
    
    Former-commit-id: df266a68cff8d5fc1ced0cd33601c6bcb3f5d235 [formerly 1f4f5741296c4a120044eb07ee8003639ef5fded] [formerly c3c02dbce5cc9dd24e1860088ee4e8d25e86d0fa [formerly 46c011ec7ed89aa32c08c66c8d62315b1ac8fc57] [formerly 7ad6c2c473e0fbcaae677fc00cfe3069a0e9fb12]]
    Former-commit-id: c3c02dbce5cc9dd24e1860088ee4e8d25e86d0fa [formerly 46c011ec7ed89aa32c08c66c8d62315b1ac8fc57]
    Former-commit-id: c3c02dbce5cc9dd24e1860088ee4e8d25e86d0fa
    Former-commit-id: acef1c97345d8915849acd8caf517b913b63b4da
    Former-commit-id: 39112622c91a709dc04c68ec496cf837a289173c
    Former-commit-id: 6aa0225f2b9b6b241abd7cd65a9fbfd952a9b997

--- msresist/figures/figureMS4.py
+++ msresist/figures/figureMS4.py
@@ -1,95 +1,69 @@
 """
-This creates Supplemental Figure 4.
+This creates Supplemental Figure 4: Predicting EGFRm/ALKf using DDMC clusters.
 """
-
+import pickle
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegressionCV
-from sklearn.cluster import KMeans
-from pomegranate import GeneralMixtureModel, NormalDistribution
+from ..logistic_regression import plotClusterCoefficients, plotROC
 from .common import subplotLabel, getSetup
-from ..pre_processing import filter_NaNpeptides
-from ..logistic_regression import plotClusterCoefficients, plotConfusionMatrix, plotROC
+from .figure3 import plotMotifs, plotUpstreamKinase_heatmap
 from .figureM3 import plot_clusters_binaryfeatures, build_pval_matrix, calculate_mannW_pvals
-from .figureM4 import find_patients_with_NATandTumor
-from .figureMS3 import plot_unclustered_LRcoef
+from .figureM4 import merge_binary_vectors, find_patients_with_NATandTumor
 
 
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((9, 20), (5, 2), multz={4: 1, 8: 1})
-
-    # Set plotting format
-    sns.set(style="whitegrid", font_scale=1.2, color_codes=True, palette="colorblind", rc={"grid.linestyle": "dotted", "axes.linewidth": 0.6})
+    ax, f = getSetup((8, 15), (5, 2), multz={0: 1, 8: 1})
 
     # Add subplot labels
     subplotLabel(ax)
 
-    # STK11 WT vs mut unclustered
-    X = pd.read_csv("msresist/data/MS/CPTAC/CPTAC-preprocessedMotfis.csv").iloc[:, 1:]
-    X = filter_NaNpeptides(X, cut=1)
-    d = X.set_index("Gene").select_dtypes(include=["float64"]).T.reset_index()
-    d.rename(columns={"index": "Patient_ID"}, inplace=True)
-    d = d.iloc[:, 1:]
+    sns.set(style="whitegrid", font_scale=1.2, color_codes=True, palette="colorblind", rc={"grid.linestyle": "dotted", "axes.linewidth": 0.6})
 
+    # Load Clustering Model from Figure 2
+    with open('msresist/data/pickled_models/binomial/CPTACmodel_BINOMIAL_CL24_W15_TMT2', 'rb') as p:
+        model = pickle.load(p)[0]
+
+    # Regression against mutation status of driver genes and clusters
     mutations = pd.read_csv("msresist/data/MS/CPTAC/Patient_Mutations.csv")
     mOI = mutations[["Sample.ID"] + list(mutations.columns)[45:54] + list(mutations.columns)[61:64]]
     y = mOI[~mOI["Sample.ID"].str.contains("IR")]
-    y_ = y["STK11.mutation.status"]
 
-    lr = LogisticRegressionCV(Cs=10, cv=15, solver="saga", max_iter=10000, n_jobs=-1, penalty="l1", class_weight="balanced")
-    plotROC(ax[0], lr, d.values, y_, cv_folds=4, title="ROC unclustered")
-    plot_unclustered_LRcoef(ax[1], lr.fit(d, y_), d)
-
-    # Run k-means
-    ncl = 24
-    x_ = X.copy()
-    x_["Cluster"] = KMeans(n_clusters=ncl).fit(d.T).labels_
-    c_kmeans = x_.groupby("Cluster").mean().T
-    c_kmeans["Patient_ID"] = X.columns[4:]
-    c_kmeans.columns = list(np.arange(ncl) + 1) + ["Patient_ID"]
+    X = pd.read_csv("msresist/data/MS/CPTAC/CPTAC-preprocessedMotfis.csv").iloc[:, 1:]
+    centers = pd.DataFrame(model.transform())
+    centers.columns = list(np.arange(model.ncl) + 1)
+    centers["Patient_ID"] = X.columns[4:]
+    centers.columns = list(np.arange(model.ncl) + 1) + ["Patient_ID"]
+    centers.iloc[:, :-1] = StandardScaler(with_std=False).fit_transform(centers.iloc[:, :-1])
 
     # Reshape data (Patients vs NAT and tumor sample per cluster)
-    c_kmeansT = find_patients_with_NATandTumor(c_kmeans.copy(), "Patient_ID", conc=True)
+    centersT = find_patients_with_NATandTumor(centers.copy(), "Patient_ID", conc=True)
     yT = find_patients_with_NATandTumor(y.copy(), "Sample.ID", conc=False)
-    assert all(c_kmeansT.index.values == yT.index.values), "Samples don't match"
+    assert all(centersT.index.values == yT.index.values), "Samples don't match"
 
-    # Regress k-means clsuters against STK11 status
-    c_kmeans["STK11"] = y["STK11.mutation.status"].values
-    c_kmeansT["STK11"] = yT["STK11.mutation.status"].values
-    c_kmeans = c_kmeans.set_index("Patient_ID")
-    plotROC(ax[2], lr, c_kmeansT.iloc[:, :-1].values, c_kmeansT["STK11"], cv_folds=4, title="ROC k-means")
-    plotClusterCoefficients(ax[3], lr.fit(c_kmeansT.iloc[:, :-1], c_kmeansT["STK11"].values), list(c_kmeansT.columns[:-1]), title="k-means")
-    pvals = calculate_mannW_pvals(c_kmeans, "STK11", 0, 1)
-    pvals = build_pval_matrix(ncl, pvals)
-    plot_clusters_binaryfeatures(c_kmeans, "STK11", ax[4], pvals=pvals)
+    # Logistic Regression
+    lr = LogisticRegressionCV(Cs=2, cv=12, solver="saga", max_iter=10000, n_jobs=-1, penalty="l1", class_weight="balanced")
 
-    # Run GMM
-    ncl = 15
-    for _ in range(10):
-        gmm = GeneralMixtureModel.from_samples(NormalDistribution, X=d.T, n_components=ncl, n_jobs=-1)
-        scores = gmm.predict_proba(d.T)
-        if np.all(np.isfinite(scores)):
-            break
-    x_["Cluster"] = gmm.predict(d.T)
-    c_gmm = x_.groupby("Cluster").mean().T
-    c_gmm["Patient_ID"] = X.columns[4:]
-    c_gmm.columns = list(np.arange(ncl) + 1) + ["Patient_ID"]
+    # EGFR mutation status
+    centers["EGFRm/ALKf"] = merge_binary_vectors(y, "EGFR.mutation.status", "ALK.fusion").values
+    centersT["EGFRm/ALKf"] = merge_binary_vectors(yT, "EGFR.mutation.status", "ALK.fusion").values
+    centers = centers.set_index("Patient_ID")
+    pvals = calculate_mannW_pvals(centers, "EGFRm/ALKf", 1, 0)
+    pvals = build_pval_matrix(model.ncl, pvals)
+    plot_clusters_binaryfeatures(centers, "EGFRm/ALKf", ["WT", "Mutant"], ax[0], pvals=pvals)
+    plotROC(ax[1], lr, centersT.iloc[:, :-1].values, centersT["EGFRm/ALKf"], cv_folds=4, title="ROC EGFRm/ALKf")
+    plotClusterCoefficients(ax[2], lr.fit(centersT.iloc[:, :-1], centersT["EGFRm/ALKf"].values), list(centersT.columns[:-1]), title="EGFRm/ALKf")
 
-    # Reshape data (Patients vs NAT and tumor sample per cluster)
-    c_gmmT = find_patients_with_NATandTumor(c_gmm.copy(), "Patient_ID", conc=True)
-    assert all(c_gmmT.index.values == yT.index.values), "Samples don't match"
+    # plot Cluster Motifs
+    pssms = model.pssms(PsP_background=False)
+    motifs = [pssms[1], pssms[12], pssms[19], pssms[22]]
+    plotMotifs(motifs, titles=["Cluster 2", "Cluster 13", "Cluster 20", "Cluster 23"], axes=ax[3:7])
 
-    # Regress GMM clusters against STK11 status
-    c_gmm["STK11"] = y["STK11.mutation.status"].values
-    c_gmmT["STK11"] = yT["STK11.mutation.status"].values
-    c_gmm = c_gmm.set_index("Patient_ID")
-    plotROC(ax[5], lr, c_gmmT.iloc[:, :-1].values, c_gmmT["STK11"], cv_folds=4, title="ROC GMM")
-    plotClusterCoefficients(ax[6], lr.fit(c_gmmT.iloc[:, :-1], c_gmmT["STK11"].values), list(c_gmmT.columns[:-1]), title="GMM")
-    pvals = calculate_mannW_pvals(c_gmm, "STK11", 0, 1)
-    pvals = build_pval_matrix(ncl, pvals)
-    plot_clusters_binaryfeatures(c_gmm, "STK11", ax[7], pvals=pvals)
+    # plot Upstream Kinases
+    plotUpstreamKinase_heatmap(model, [1, 2, 8, 12, 13, 20, 23], ax[7])
 
     return f
