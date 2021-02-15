15c4570c09652e5b49c52663700304a5ee554ce4
Marc Creixell <creixell@g.ucla.edu>
2021-02-15 15:46:24 +0100

    delete Tregs and format figM5. (#349)
    * delete Tregs and format plot.
    
    * fix.
    
    * don't delete Tregs.
    
    Co-authored-by: marcc <creixell@ucla.edu>
    Former-commit-id: e3801b87bb58dc5b33a91d7688c89eaa9717f43b [formerly b0638af8787ae49f56686aff287ef0429e1c6536] [formerly 68b2ce868da8de864e5405280e4345ae8209cad7]
    Former-commit-id: b8d1308105151b22a68d4889caaf4dbd3c2aa264
    Former-commit-id: 707d8080fcc5a5ef68664a5a70900e7eb8097e20
    Former-commit-id: f3245ab235d22a938731dff79e20f1351c20eb2b

--- msresist/figures/figureM5.py
+++ msresist/figures/figureM5.py
@@ -20,7 +20,7 @@ from .figureM4 import find_patients_with_NATandTumor
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((15, 7), (2, 4), multz={2: 1, 7: 1})
+    ax, f = getSetup((15, 7), (2, 4), multz={2:1, 6: 1})
 
     # Set plotting format
     sns.set(style="whitegrid", font_scale=1.2, color_codes=True, palette="colorblind", rc={"grid.linestyle": "dotted", "axes.linewidth": 0.6})
@@ -60,11 +60,11 @@ def makeFigure():
 
     # plot Cluster Motifs
     pssms = model.pssms(PsP_background=False)
-    motifs = [pssms[1], pssms[2], pssms[16]]
-    plotMotifs(motifs, titles=["Cluster 2", "Cluster 3", "Cluster 17"], axes=ax[3:6])
+    motifs = [pssms[4], pssms[18]]
+    plotMotifs(motifs, titles=["Cluster 5", "Cluster 19"], axes=ax[3:5])
 
     # plot Upstream Kinases
-    plotUpstreamKinase_heatmap(model, [2, 3, 17], ax[6])
+    plotUpstreamKinase_heatmap(model, [5, 19], ax[5])
 
     return f
 
@@ -74,7 +74,7 @@ def plot_LassoCoef_Immune(ax, reg, centers, y, ncl, s_type="Tumor"):
     # Format data for seaborn
     coef = pd.DataFrame(reg.coef_.T)
     coef.columns = y.columns
-    coef["Cluster"] = list(np.arange(24)) * 2
+    coef["Cluster"] = list(np.arange(24) + 1) * 2
     coef["Sample"] = ["Tumor"] * ncl + ["NAT"] * ncl
     coef = pd.melt(coef, id_vars=["Cluster", "Sample"], value_vars=list(coef.columns[:-2]), var_name=["Cell Line"], value_name="Coefficient")
 
@@ -85,6 +85,8 @@ def plot_LassoCoef_Immune(ax, reg, centers, y, ncl, s_type="Tumor"):
     else:
         ax.set_title("Tumor and NAT samples driving Infiltration")
         sns.catplot(x="Cluster", y="Coefficient", hue="Cell Line", col="Sample", kind="bar", data=coef, ax=ax, **{"linewidth": 0.2}, **{"edgecolor": "black"})
+
+    ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0, labelspacing=0.2) #Legend out
 
     # Add r2 coef
     textstr = "$r2 score$ = " + str(np.round(r2_score(y, reg.predict(centers)), 4))
