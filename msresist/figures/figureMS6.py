fe05d29de6808ae51c696a5aaf64ab5737f54dad
marcc <creixell@ucla.edu>
2021-02-22 10:46:29 -0800

    update figMS6.svg

--- msresist/figures/figureMS6.py
+++ msresist/figures/figureMS6.py
@@ -43,24 +43,26 @@ def makeFigure():
     lr = LogisticRegressionCV(Cs=10, cv=10, solver="saga", max_iter=10000, n_jobs=-1, penalty="l1", class_weight="balanced")
 
     folds = 7
-    for idx, PathToModel in enumerate(models):
+    ii = np.arange(0, 13, 3)
+    for jj, PathToModel in enumerate(models):
         with open(PathToModel, 'rb') as m:
             model = pickle.load(m)[0]
 
-        print(model.SeqWeight)
+        IDX = ii[jj]
+
         # Find and scale centers
-        centers = TransformCenters(model, X)
+        centers_gen, centers_hcb = TransformCenters(model, X)
 
         #STK11
-        plotROC(ax[idx], lr, centers.values, y["STK11.mutation.status"], cv_folds=folds, title="STK " + "w=" + str(model.SeqWeight))
+        plotROC(ax[IDX], lr, centers_gen.values, y["STK11.mutation.status"], cv_folds=folds, title="STK " + "w=" + str(model.SeqWeight))
 
         #EGFRm/ALKf
         y_EA = merge_binary_vectors(y.copy(), "EGFR.mutation.status", "ALK.fusion")
-        plotROC(ax[idx + 1], lr, centers.values, y_EA, cv_folds=folds, title="EGFRm/ALKf " + "w=" + str(model.SeqWeight))
+        plotROC(ax[IDX + 1], lr, centers_gen.values, y_EA, cv_folds=folds, title="EGFRm/ALKf " + "w=" + str(model.SeqWeight))
 
         #Hot-Cold behavior
-        y_hcb = HotColdBehavior(centers)
-        plotROC(ax[idx + 2], lr, centers.values, y_hcb, cv_folds=folds, title="Infiltration " + "w=" + str(model.SeqWeight))
+        y_hcb, centers_hcb = HotColdBehavior(centers_hcb)
+        plotROC(ax[IDX + 2], lr, centers_hcb.values, y_hcb, cv_folds=folds, title="Infiltration " + "w=" + str(model.SeqWeight))
 
     return f
 
@@ -72,19 +74,19 @@ def TransformCenters(model, X):
     centers = centers.T
     centers.columns = np.arange(model.ncl) + 1
     centers["Patient_ID"] = X.columns[4:]
-    centers = find_patients_with_NATandTumor(centers.copy(), "Patient_ID", conc=True)
-    return centers
+    centers1 = find_patients_with_NATandTumor(centers.copy(), "Patient_ID", conc=True)
+    centers2 = centers.loc[~centers["Patient_ID"].str.endswith(".N"), :].sort_values(by="Patient_ID").set_index("Patient_ID")
+    return centers1, centers2
 
 
 def HotColdBehavior(centers):
     # Import Cold-Hot Tumor data
-    centers = centers.loc[~centers["Patient_ID"].str.endswith(".N"), :].sort_values(by="Patient_ID").set_index("Patient_ID")
     y = pd.read_csv("msresist/data/MS/CPTAC/Hot_Cold.csv").dropna(axis=1).sort_values(by="Sample ID")
     y = y.loc[~y["Sample ID"].str.endswith(".N"), :].set_index("Sample ID")
     l1 = list(centers.index)
     l2 = list(y.index)
     dif = [i for i in l1 + l2 if i not in l1 or i not in l2]
-    centers = y.drop(dif)
+    centers = centers.drop(dif)
 
     # Transform to binary
     y = y.replace("Cold-tumor enriched", 0)
@@ -94,5 +96,6 @@ def HotColdBehavior(centers):
     # Remove NAT-enriched samples
     centers = centers.drop(y[y == "NAT enriched"].index)
     y = y.drop(y[y == "NAT enriched"].index).astype(int)
+    assert all(centers.index.values == y.index.values), "Samples don't match"
 
-    return y
+    return y, centers
