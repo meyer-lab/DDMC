e7287b7487440543bd4c9ebb3f50c4928a04a2e6
Marc Creixell <creixell@g.ucla.edu>
2021-02-23 18:14:59 +0100

    MS6-Compare predictions with varying weights (#363)
    * MS6
    
    * update figMS6.svg
    
    * swap M2 and MS6.
    
    * fix imports.
    
    * update genfigure
    
    * switch to ROC and remove labels.
    
    * fix.
    
    Co-authored-by: marcc <creixell@ucla.edu>
    Former-commit-id: 07ec43ac32bf49cc28eed59a056d416317af091f [formerly a4a1634f0993063f28f9f99a5d5fe85a0e50f222] [formerly 8f55c4de425d9bbfd95e908550f7ad771ae1284a [formerly 3ddd76e4208b5ee387fa31727b8ba2c42b6f8734] [formerly f648b32b7e5200b099a31ad6b3729020dbbdc7e2]]
    Former-commit-id: 8f55c4de425d9bbfd95e908550f7ad771ae1284a [formerly 3ddd76e4208b5ee387fa31727b8ba2c42b6f8734]
    Former-commit-id: 8f55c4de425d9bbfd95e908550f7ad771ae1284a
    Former-commit-id: 234567df20e4cb4940874cc5d5dd255b4f7d6086
    Former-commit-id: ea8ea4dda3d9f3f0eb3be981158229cac4491cc0
    Former-commit-id: 14f975eb0ab46c46be871355e931a0b1acb77c1b

--- msresist/logistic_regression.py
+++ msresist/logistic_regression.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from scipy.stats import sem
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import auc
 from sklearn.metrics import plot_roc_curve
@@ -59,11 +60,9 @@ def plotROC(ax, classifier, d, y, cv_folds=4, title=False):
     aucs = []
     mean_fpr = np.linspace(0, 1, 100)
 
-    for i, (train, test) in enumerate(cv.split(d, y)):
+    for _, (train, test) in enumerate(cv.split(d, y)):
         classifier.fit(d[train], y[train])
-        viz = plot_roc_curve(classifier, d[test], y[test],
-                             name='ROC fold {}'.format(i),
-                             alpha=0.3, lw=1, ax=ax)
+        viz = plot_roc_curve(classifier, d[test], y[test], name="", alpha=0.0, lw=1)
         interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
         interp_tpr[0] = 0.0
         tprs.append(interp_tpr)
@@ -73,16 +72,16 @@ def plotROC(ax, classifier, d, y, cv_folds=4, title=False):
     mean_tpr = np.mean(tprs, axis=0)
     mean_tpr[-1] = 1.0
     mean_auc = auc(mean_fpr, mean_tpr)
-    std_auc = np.std(aucs)
+    sem_auc = sem(aucs)
     ax.plot(mean_fpr, mean_tpr, color='b',
-            label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+            label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, sem_auc),
             lw=2, alpha=.8)
 
-    std_tpr = np.std(tprs, axis=0)
-    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    sem_tpr = sem(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + sem_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - sem_tpr, 0)
     ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
-                    label=r'$\pm$ 1 std. dev.')
+                    label=r'$\pm$ 1 SEM')
 
     ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
            title="ROC")
