4246b6f1506b761170f25a6156d0bbba4d94dd1f
Marc Creixell <creixell@g.ucla.edu>
2021-03-18 11:52:33 -0700

    check variation in cluster centers. (#385)
    Former-commit-id: d9bcf4a01da5fb6fbb6bc3167e98cf8e3eed3033 [formerly 5c72b41edf2902b2240086851115533259d41899] [formerly 75edfa93d42d94d973b1b00f8b9e631b6d64703d [formerly eab7b6258d20b4360809c0b48f8d13fdb759353a] [formerly 6ee489488c4bf5cc0ea5f8581e24e4ac8108d40b]]
    Former-commit-id: 75edfa93d42d94d973b1b00f8b9e631b6d64703d [formerly eab7b6258d20b4360809c0b48f8d13fdb759353a]
    Former-commit-id: 75edfa93d42d94d973b1b00f8b9e631b6d64703d
    Former-commit-id: 3823b7d30a48bb252d05087bb691f655861243d9
    Former-commit-id: e0bc225f68b0e0bfdc9f6d73c4eaef9c2d7d109d
    Former-commit-id: 2844ee3e848252ed6ab4d6f09d137bfb4e3955c1

---
+++ msresist/tests/test_DifClusters.py
@@ -0,0 +1,22 @@
+"""
+Testing file for minimum variance across clusters using CPTAC data.
+"""
+
+import pytest
+import numpy as np
+import pandas as pd
+from ..clustering import MassSpecClustering
+from ..pre_processing import filter_NaNpeptides
+
+X = pd.read_csv("msresist/data/MS/CPTAC/CPTAC-preprocessedMotfis.csv").iloc[:, 1:]
+X = filter_NaNpeptides(X, tmt=25)
+d = X.select_dtypes(include=['float64']).T
+i = X.select_dtypes(include=['object'])
+preMotifSet = ["ABL", "EGFR", "ALK", "SRC", "YES"]
+
+@pytest.mark.parametrize("distance_method", ["PAM250", "Binomial", "PAM250_fixed"])
+def test_ClusterVar(distance_method):
+    """Test minimum variance of output cluster centers """
+    model = MassSpecClustering(i, 5, SeqWeight=3, distance_method=distance_method, pre_motifs=preMotifSet).fit(X=d)
+    centers = model.transform()
+    assert np.mean(np.std(centers, axis=1)) > 0.2
