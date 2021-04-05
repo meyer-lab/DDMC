ba651bff47c574e55f22e61d303c783d3d81922f
Marc Creixell <creixell@g.ucla.edu>
2021-04-04 23:42:57 -0700

    blots.

--- genFigure.py
+++ genFigure.py
@@ -42,14 +42,14 @@ if __name__ == "__main__":
         overlayCartoon(fdir + 'figure2.svg',
                        f'{cartoon_dir}/AXL_MS_heatmap.svg', 10, 0, scalee=0.10)
 
-    if sys.argv[1] == "4":
-        # Overlay Figure 4 YAP AXL vs KO blot
-        overlayCartoon(fdir + 'figure4.svg',
+    if sys.argv[1] == "3":
+        # Overlay Figure 3 YAP AXL vs KO blot
+        overlayCartoon(fdir + 'figure3.svg',
                        f'{cartoon_dir}/YAP_WTvsKO_blot.svg', 700, 590, scalee=0.17)
 
-    if sys.argv[1] == "4":
-        # Overlay Figure 4 YAP dasatinib dose response
-        overlayCartoon(fdir + 'figure4.svg',
+    if sys.argv[1] == "3":
+        # Overlay Figure 3 YAP dasatinib dose response
+        overlayCartoon(fdir + 'figure3.svg',
                        f'{cartoon_dir}/YAP_DR_blot.svg', 50, 600, scalee=0.17)
 
     if sys.argv[1] == "S6":
