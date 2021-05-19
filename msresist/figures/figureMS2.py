b04c9c358e7cf7a9481969afa5d67270d31da66e
Marc Creixell <creixell@g.ucla.edu>
2021-05-19 11:51:42 -0700

    reduce size motifs. (#437)
    Co-authored-by: marcc <creixell@ucla.edu>
    Former-commit-id: fe315b25c44455361574b0203a6a1885c866de97 [formerly 7d3ecfdef5af84425e574ae10b4563c7992f8895] [formerly 4ab820ab60ce3fe8618d247ec79a3c019d22185f [formerly ae10b10d9d8c6db307104719c538a3f4f27e05f9] [formerly a25a7a6e1aa934b079c07803ad05536840061d50]]
    Former-commit-id: 4ab820ab60ce3fe8618d247ec79a3c019d22185f [formerly ae10b10d9d8c6db307104719c538a3f4f27e05f9]
    Former-commit-id: 4ab820ab60ce3fe8618d247ec79a3c019d22185f
    Former-commit-id: fdb515ff45a7e677f74f80b7511b2a01b7601ce5
    Former-commit-id: e5d7e0b9f264d452ff20bdac3057f8a092aef8ae
    Former-commit-id: db49a0e19c35acb43e0c62210bb7f0e463bdd569

--- msresist/figures/figureMS2.py
+++ msresist/figures/figureMS2.py
@@ -3,6 +3,7 @@ This creates Supplemental Figure 2: Cluster motifs
 """
 
 import pickle
+import numpy as np 
 from .common import subplotLabel, getSetup
 from .figure2 import plotMotifs
 
@@ -10,15 +11,23 @@ from .figure2 import plotMotifs
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((7, 11), (6, 4))
+    ax, f = getSetup((7, 10), (6, 4))
 
     with open('msresist/data/pickled_models/binomial/CPTACmodel_BINOMIAL_CL24_W15_TMT2', 'rb') as p:
         model = pickle.load(p)[0]
 
     pssms = model.pssms(PsP_background=False)
+    ylabels = np.arange(0, 21, 4)
+    xlabels = [20, 21, 22, 23, 24]
     for ii in range(model.ncl):
         cluster = "Cluster " + str(ii + 1)
         plotMotifs([pssms[ii]], axes=[ax[ii]], titles=[cluster], yaxis=[0, 10])
+        if ii not in ylabels:
+            ax[ii].set_ylabel("")
+            ax[ii].get_yaxis().set_visible(False)
+        elif ii not in xlabels:
+            ax[ii].set_xlabel("")
+            ax[ii].get_xaxis().set_visible(False)
 
     # Add subplot labels
     subplotLabel(ax)
