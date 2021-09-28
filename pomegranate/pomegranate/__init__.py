3cf59c1e67e793a9f994a15cbf16fc6340981efb
aarmey <aarmey@users.noreply.github.com>
2021-09-28 18:01:44 +0000

    autopep8 action fixes

--- pomegranate/pomegranate/__init__.py
+++ pomegranate/pomegranate/__init__.py
@@ -19,32 +19,33 @@ __version__ = '0.14.5'
 os.environ['OMP_NUM_THREADS'] = '1'
 os.environ['MKL_NUM_THREADS'] = '1'
 
+
 def from_json(s):
-	"""A robust loading method.
+    """A robust loading method.
 
-	This method can load an appropriately formatted JSON object from any model
-	in pomegranate and return the appropriate object. This relies mostly on the
-	'class' attribute in the JSON.
+    This method can load an appropriately formatted JSON object from any model
+    in pomegranate and return the appropriate object. This relies mostly on the
+    'class' attribute in the JSON.
 
-	Parameters
-	----------
-	s : str
-		Either the filename of a JSON object or a string that is JSON formatted,
-		as produced by any of the `to_json` methods in pomegranate.
-	"""
+    Parameters
+    ----------
+    s : str
+            Either the filename of a JSON object or a string that is JSON formatted,
+            as produced by any of the `to_json` methods in pomegranate.
+    """
 
-	try:
-		d = json.loads(s)
-	except:
-		try:
-			with open(s, 'r') as f:
-				d = json.load(f)
-		except:
-			raise IOError("String must be properly formatted JSON or filename of properly formatted JSON.")
+    try:
+        d = json.loads(s)
+    except BaseException:
+        try:
+            with open(s, 'r') as f:
+                d = json.load(f)
+        except BaseException:
+            raise IOError("String must be properly formatted JSON or filename of properly formatted JSON.")
 
-	if d['class'] == 'Distribution':
-		return Distribution.from_json(s)
-	elif d['class'] == 'GeneralMixtureModel':
-		return GeneralMixtureModel.from_json(s)
-	else:
-		raise ValueError("Must pass in an JSON with a valid model name.")
+    if d['class'] == 'Distribution':
+        return Distribution.from_json(s)
+    elif d['class'] == 'GeneralMixtureModel':
+        return GeneralMixtureModel.from_json(s)
+    else:
+        raise ValueError("Must pass in an JSON with a valid model name.")
