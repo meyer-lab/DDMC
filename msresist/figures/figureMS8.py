52059dc9c0407ecaf00e915782db1d97ec5a7eb4
Marc Creixell <creixell@g.ucla.edu>
2021-11-22 11:17:20 -0800

    Figure 6 and S6 (#522)
    * edits.
    
    * .
    
    * updatefigures.

--- msresist/figures/figureMS8.py
+++ msresist/figures/figureMS8.py
@@ -1,5 +1,5 @@
 """
-This creates Supplemental Figure 7: Predicting STK11 genotype using different clustering strategies.
+This creates Supplemental Figure 7: Adjusted Mutual Information across clustering methods 
 """
 
 import numpy as np
