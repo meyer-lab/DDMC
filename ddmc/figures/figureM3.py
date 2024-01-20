0a86267bbcead8efc688f850c7351b65ec554b99
Aaron Meyer <ameyer@ucla.edu>
2024-01-20 07:12:57 -0800

    Remove unused code and data files

--- ddmc/figures/figureM3.py
+++ ddmc/figures/figureM3.py
@@ -7,13 +7,13 @@ import pandas as pd
 import seaborn as sns
 import matplotlib
 from ..clustering import DDMC
-from ..validations import preprocess_ebdt_mcf7
+from ..binomial import AAlist
 from .common import subplotLabel, getSetup
 from ..pca import plotPCA
-from .common import plotDistanceToUpstreamKinase, plotMotifs, ShuffleClusters
-from .figureM5 import plot_NetPhoresScoreByKinGroup
+from .common import plotDistanceToUpstreamKinase, plotMotifs
 from ..clustering import compute_control_pssm
 from ..binomial import AAlist
+from ..motifs import DictProteomeNameToSeq
 from ..pre_processing import filter_NaNpeptides
 
 
@@ -118,6 +118,66 @@ def makeFigure():
     return f
 
 
+def ShuffleClusters(shuffle, model, additional=False):
+    """Returns PSSMs with shuffled positions"""
+    ClustersToShuffle = np.array(shuffle)
+    pssms, _ = model.pssms(PsP_background=False)
+    s_pssms = []
+    for s in ClustersToShuffle:
+        mat = ShufflePositions(pssms[s])
+        s_pssms.append(mat)
+
+    if not isinstance(additional, bool):
+        mat = ShufflePositions(additional)
+        s_pssms.append(mat)
+
+    return s_pssms
+
+
+def ShufflePositions(pssm):
+    """Shuffles the positions of input PSSMs"""
+    pssm = np.array(pssm)
+    mat = pssm[:, np.random.permutation([0, 1, 2, 3, 4, 6, 7, 8, 9])]
+    mat = np.insert(mat, 5, pssm[:, 5], axis=1)
+    mat = np.insert(mat, 1, pssm[:, -1], axis=1)
+    mat = pd.DataFrame(mat)
+    mat.index = AAlist
+    return mat
+
+
+def plot_NetPhoresScoreByKinGroup(PathToFile, ax, n=5, title=False, color="royalblue"):
+    """Plot top scoring kinase groups"""
+    NPtoCumScore = {}
+    X = pd.read_csv(PathToFile)
+    for ii in range(X.shape[0]):
+        curr_NPgroup = X["netphorest_group"][ii]
+        if curr_NPgroup == "any_group":
+            continue
+        elif curr_NPgroup not in NPtoCumScore.keys():
+            NPtoCumScore[curr_NPgroup] = X["netphorest_score"][ii]
+        else:
+            NPtoCumScore[curr_NPgroup] += X["netphorest_score"][ii]
+    X = pd.DataFrame.from_dict(NPtoCumScore, orient="index").reset_index()
+    X.columns = ["KIN Group", "NetPhorest Score"]
+    X["KIN Group"] = [s.split("_")[0] for s in X["KIN Group"]]
+    X = X.sort_values(by="NetPhorest Score", ascending=False).iloc[:n, :]
+    sns.stripplot(
+        data=X,
+        y="KIN Group",
+        x="NetPhorest Score",
+        ax=ax,
+        orient="h",
+        color=color,
+        size=5,
+        **{"linewidth": 1},
+        **{"edgecolor": "black"},
+    )
+    if title:
+        ax.set_title(title)
+    else:
+        ax.set_title("Kinase Predictions")
+
+
 def plotMCF7AKTclustermap(model, cluster):
     """Code to create hierarchical clustering of cluster 1 across treatments"""
     c1 = pd.DataFrame(model.transform()[:, cluster - 1])
@@ -139,3 +199,51 @@ def plotMCF7AKTclustermap(model, cluster):
         yticklabels=True,
         xticklabels=False,
     )
+
+
+def preprocess_ebdt_mcf7():
+    """Preprocess MCF7 mass spec data set from EBDT (Hijazi et al Nat Biotech 2020)"""
+    x = (
+        pd.read_csv("ddmc/data/Validations/Computational/ebdt_mcf7.csv")
+        .drop("FDR", axis=1)
+        .set_index("sh.index.sites")
+        .drop("ARPC2_HUMAN;")
+        .reset_index()
+    )
+    x.insert(0, "Gene", [s.split("(")[0] for s in x["sh.index.sites"]])
+    x.insert(
+        1,
+        "Position",
+        [re.search(r"\(([A-Za-z0-9]+)\)", s).group(1) for s in x["sh.index.sites"]],
+    )
+    x = x.drop("sh.index.sites", axis=1)
+    motifs, del_ids = pos_to_motif(x["Gene"], x["Position"], motif_size=5)
+    x = x.set_index(["Gene", "Position"]).drop(del_ids).reset_index()
+    x.insert(0, "Sequence", motifs)
+    return x
+
+
+def pos_to_motif(genes, pos, motif_size=5):
+    """Map p-site sequence position to uniprot's proteome and extract motifs."""
+    proteome = open("ddmc/data/Sequence_analysis/proteome_uniprot2019.fa", "r")
+    ProteomeDict = DictProteomeNameToSeq(proteome, n="gene")
+    motifs = []
+    del_GeneToPos = []
+    for gene, pos in list(zip(genes, pos)):
+        try:
+            UP_seq = ProteomeDict[gene]
+        except BaseException:
+            del_GeneToPos.append([gene, pos])
+            continue
+        idx = int(pos[1:]) - 1
+        motif = list(UP_seq[max(0, idx - motif_size) : idx + motif_size + 1])
+        if (
+            len(motif) != motif_size * 2 + 1
+            or pos[0] != motif[motif_size]
+            or pos[0] not in ["S", "T", "Y"]
+        ):
+            del_GeneToPos.append([gene, pos])
+            continue
+        motif[motif_size] = motif[motif_size].lower()
+        motifs.append("".join(motif))
+    return motifs, del_GeneToPos
