d6c7fe091d6c445a7d1daa5154aa71178325048b
Armaan Abraham <armaanabraham@g.ucla.edu>
2024-02-01 20:24:54 -0800

    figure S3

--- ddmc/figures/figureMS3.py
+++ ddmc/figures/figureMS3.py
@@ -1,82 +1,77 @@
-"""
-This creates Supplemental Figure 3: Predictive performance of DDMC clusters using different weights
-"""
-
-import pandas as pd
 from sklearn.linear_model import LogisticRegressionCV
-from .common import getSetup, getDDMC_CPTAC
-from .figureM4 import transform_centers, get_hot_cold_labels, find_patients_with_NATandTumor
-from ..logistic_regression import plotROC
+from sklearn.preprocessing import StandardScaler
 
+from ddmc.clustering import DDMC
+from ddmc.datasets import CPTAC, select_peptide_subset
+from ddmc.figures.common import getSetup
+from ddmc.logistic_regression import plotROC
 
 def makeFigure():
-    """Get a list of the axis objects and create a figure"""
-    # Get list of axis objects
-    ax, f = getSetup((15, 10), (3, 5))
+    cptac = CPTAC()
 
-    # Genotype data
-    mutations = pd.read_csv("ddmc/data/MS/CPTAC/Patient_Mutations.csv")
-    mOI = mutations[
-        ["Sample.ID"] + list(mutations.columns)[45:54] + list(mutations.columns)[61:64]
-    ]
-    y = mOI[~mOI["Sample.ID"].str.contains("IR")]
-    y = find_patients_with_NATandTumor(y.copy(), "Sample.ID", conc=False)
+    mutations = cptac.get_mutations(
+        ["STK11.mutation.status", "EGFR.mutation.status", "ALK.fusion"]
+    )
+    stk11 = mutations["STK11.mutation.status"]
+    egfr = mutations["EGFR.mutation.status"]
+    hot_cold = cptac.get_hot_cold_labels()
+
+    p_signal = select_peptide_subset(CPTAC().get_p_signal(), keep_num=100)
 
     # LASSO
     lr = LogisticRegressionCV(
         Cs=10,
         cv=10,
         solver="saga",
-        max_iter=10000,
+        max_iter=1000,
         n_jobs=-1,
         penalty="l1",
         class_weight="balanced",
     )
 
-    folds = 5
-    weights = [0, 100, 500, 1000, 1000000]
-    for ii, w in enumerate(weights):
-        model, X = getDDMC_CPTAC(n_components=30, SeqWeight=w)
+    folds = 3
+    weights = [0, 500, 1000000]
+    ax, f = getSetup((15, 10), (3, len(weights)))
+    for ii, weight in enumerate(weights):
+        model = DDMC(
+            n_components=30,
+            seq_weight=weight,
+            distance_method="Binomial",
+            random_state=5,
+            max_iter=10,
+        ).fit(p_signal)
 
         # Find and scale centers
-        centers_gen, centers_hcb = transform_centers(model, X)
-
-        if w == 0:
-            prio = " (data only)"
-        elif w == 50:
-            prio = " (motif mainly)"
-        else:
-            prio = " (mix)"
+        centers = model.transform(as_df=True)
+        centers.iloc[:, :] = StandardScaler(with_std=False).fit_transform(centers.values)
 
         # STK11
         plotROC(
-            ax[ii],
             lr,
-            centers_gen.values,
-            y["STK11.mutation.status"].values, # type: ignore
+            centers.loc[stk11.index].values,
+            stk11,
             cv_folds=folds,
-            title="STK11m " + "w=" + str(model.seq_weight) + prio,
+            return_mAUC=False,
+            ax=ax[ii],
+            title="STK11m " + "w=" + str(model.seq_weight),
         )
-
-        # EGFRm
         plotROC(
-            ax[ii + 5],
             lr,
-            centers_gen.values,
-            y["EGFR.mutation.status"].values, # type: ignore
+            centers.loc[egfr.index].values,
+            egfr,
             cv_folds=folds,
-            title="EGFRm " + "w=" + str(model.seq_weight) + prio,
+            return_mAUC=False,
+            ax=ax[ii + len(weights)],
+            title="EGFRm " + "w=" + str(model.seq_weight),
         )
-
-        # Hot-Cold behavior
-        y_hcb, centers_hcb = get_hot_cold_labels(centers_hcb)
         plotROC(
-            ax[ii + 10],
             lr,
-            centers_hcb.values,
-            y_hcb.values,
+            centers.loc[hot_cold.index].values,
+            hot_cold,
             cv_folds=folds,
-            title="Infiltration " + "w=" + str(model.seq_weight) + prio,
+            return_mAUC=False,
+            ax=ax[ii + len(weights) * 2],
+            title="Infiltration " + "w=" + str(model.seq_weight),
         )
 
     return f
